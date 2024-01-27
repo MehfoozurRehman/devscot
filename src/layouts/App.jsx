6ce66bdeaa4729c0c002544dc3dace2aabdc33eb
M Abrar Khalid <100939524+abrarkhalidofficial@users.noreply.github.com>
2024-01-27 12:25:17 +0500

    base done

---
+++ src/layouts/App.jsx
@@ -0,0 +1,16 @@
+import { Head } from '@router';
+import { Outlet } from 'react-router-dom';
+
+export default () => {
+  return (
+    <>
+      <Head
+        title="Devscot"
+        image="/favicon.ico"
+        url="https://vitefilerouter.com"
+        description="Devscot"
+      />
+      <Outlet />
+    </>
+  );
+};
