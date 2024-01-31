b3ceac0536d046c23d8178b09ba84644ac6e0d73
M Abrar Khalid <100939524+abrarkhalidofficial@users.noreply.github.com>
2024-01-31 11:19:04 +0500

    Update index.ts

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,4 +1,11 @@
+import PngHerosection from './herosection.png'
+import PngHeroshape from './heroshape.png'
+import PngHeroshapearrow from './heroshapearrow.png'
+import PngHeroshapestar from './heroshapestar.png'
 
 export {
-
+  PngHerosection,
+  PngHeroshape,
+  PngHeroshapearrow,
+  PngHeroshapestar
 }
