dc2fc585f8031b5bb0d637d4fe4c91874bddbd84
M Abrar Khalid <100939524+abrarkhalidofficial@users.noreply.github.com>
2024-02-24 10:15:57 +0500

    Create Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,140 @@
+import React, { useEffect, useState } from "react";
+import headerlogo from "../assets/headerlogo.png";
+import ClickAwayListener from "react-click-away-listener";
+import { NavLink } from "react-router-dom";
+import { Menu, X } from "react-feather";
+
+export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  function changeNavState() {
+    if (window.innerWidth < 1000) {
+      setIsOpen(false);
+    } else {
+      setIsOpen(true);
+    }
+  }
+  useEffect(() => {
+    changeNavState();
+    window.addEventListener("resize", changeNavState);
+    window.addEventListener("scroll", () => {
+      changeNavState();
+    });
+  }, []);
+
+  return (
+    <div
+      className="header"
+      style={
+        isOpen && window.innerWidth < 1000
+          ? { backgroundColor: "#fffff" }
+          : null
+      }
+    >
+      <div className="header__content">
+        <NavLink to="/" className="header__content__logo">
+          <img src={headerlogo} alt="logo" />
+        </NavLink>
+
+        {isOpen ? (
+          <ClickAwayListener
+            onClickAway={() => {
+              if (window.innerWidth < 1000) {
+                setIsOpen(false);
+              }
+            }}
+          >
+            <div className="header__content__nav">
+              <NavLink
+                to="/"
+                activeclassname="active"
+                className="header__content__nav__link"
+                onClick={() => {
+                  if (window.innerWidth < 1000) {
+                    setIsOpen(false);
+                  }
+                }}
+              >
+                Home
+              </NavLink>
+
+              <NavLink
+                to="/"
+                activeclassname="active"
+                className="header__content__nav__link"
+                onClick={() => {
+                  if (window.innerWidth < 1000) {
+                    setIsOpen(false);
+                  }
+                }}
+              >
+                Services
+              </NavLink>
+
+              <NavLink
+                to="/"
+                activeclassname="active"
+                className="header__content__nav__link"
+                onClick={() => {
+                  if (window.innerWidth < 1000) {
+                    setIsOpen(false);
+                  }
+                }}
+              >
+                Portfolio
+              </NavLink>
+
+              <NavLink
+                to="/"
+                activeclassname="active"
+                className="header__content__nav__link"
+                onClick={() => {
+                  if (window.innerWidth < 1000) {
+                    setIsOpen(false);
+                  }
+                }}
+              >
+                About Us
+              </NavLink>
+
+              <NavLink
+                to="/pricing"
+                onClick={() => {
+                  if (window.innerWidth < 1000) {
+                    setIsOpen(false);
+                  }
+                }}
+                className="header__content__cta__button"
+              >
+                Lets Talk
+              </NavLink>
+            </div>
+          </ClickAwayListener>
+        ) : null}
+
+        <div className="header__content__cta">
+          <button
+            className="header__content__menu"
+            onClick={() => {
+              setIsOpen(!isOpen);
+            }}
+            title={isOpen ? "Close" : "Open"}
+          >
+            {isOpen ? (
+              <X size={20} color="currentColor" />
+            ) : (
+              <Menu size={20} color="currentColor" />
+            )}
+          </button>
+
+          <NavLink
+            to="/pricing"
+            title="Sign Up"
+            className="header__content__cta__button"
+          >
+            Sign Up
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
+}
