adfaf147aec5e032420698a1a8e30efbc8819555
Arslan Goursi <arsljiger431@gmail.com>
2024-02-26 17:47:40 +0500

    Update index.ts

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,7 +1,11 @@
+import PngDevscot1 from './devscot1.png'
+import PngDevscot2 from './devscot2.png'
 import PngHeaderlogo from './headerlogo.png'
 import PngHerobackground from './herobackground.png'
 
 export {
+  PngDevscot1,
+  PngDevscot2,
   PngHeaderlogo,
   PngHerobackground
 }
