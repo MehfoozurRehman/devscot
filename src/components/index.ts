33d5d081a171bbde271561dfca97a6b445a0b07b
Arslan Goursi <arsljiger431@gmail.com>
2024-02-26 17:47:42 +0500

    Update index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,9 @@
 import Header from './Header.jsx'
+import NumbersSection from './NumbersSection.jsx'
 import SolutionSection from './SolutionSection.jsx'
 
 export {
   Header,
+  NumbersSection,
   SolutionSection
 }
