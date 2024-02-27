9b3de5f9308f023521f1da978c3c024e383d8a99
Arslan Goursi <arsljiger431@gmail.com>
2024-02-27 12:40:17 +0500

    package

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,11 @@
 import Header from './Header.jsx'
+import HowWorkSection from './HowWorkSection.jsx'
 import NumbersSection from './NumbersSection.jsx'
 import SolutionSection from './SolutionSection.jsx'
 
 export {
   Header,
+  HowWorkSection,
   NumbersSection,
   SolutionSection
 }
