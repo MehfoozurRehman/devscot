0cfe8142e3b6803783ff007d87eec9d14974ec5d
Arslan Goursi <arsljiger431@gmail.com>
2024-02-27 13:50:17 +0500

    Update HowWorkSection.jsx

--- src/components/HowWorkSection.jsx
+++ src/components/HowWorkSection.jsx
@@ -1,13 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+
 import ScrollContainer from "react-indiana-drag-scroll";
 
 export default function HowWorkSection() {
+  const [isMouseHover, setIsMouseHover] = useState("03");
   const entries = [
     {
       step: "Step 01",
       number: "01",
       heading: "Audience",
       text: "Having these the marketplace to your business",
+    },
+    {
+      step: "Step 02",
+      number: "02",
+      heading: "Plan & Sketch",
+      text: "Delicate and long-lasting with vitamins and nutritions",
+    },
+    {
+      step: "Step 03",
+      number: "03",
+      heading: "Customize",
+      text: "Creating brand identities for the digital experiences",
+    },
+    {
+      step: "Step 04",
+      number: "04",
+      heading: "User Testing",
+      text: "We look forward to engage with beyond the conventional",
+    },
+    {
+      step: "Step 05",
+      number: "05",
+      heading: "User Testing",
+      text: "We look forward to engage with beyond the conventional",
+    },
+    {
+      step: "Step 06",
+      number: "06",
+      heading: "User Testing",
+      text: "We look forward to engage with beyond the conventional",
     },
   ];
 
@@ -18,10 +50,45 @@ export default function HowWorkSection() {
         <div className="how__section__content__heading">How we work</div>
         <ScrollContainer>
           <div className="how__section__content__data">
-            <WorkFlowEntry />
-            <WorkFlowEntry />
-            <WorkFlowEntry />
-            <WorkFlowEntry />
+            {entries.map((entry, index) => (
+              <div
+                className="how__section__content__data__entry"
+                key={index}
+                onMouseEnter={() => setIsMouseHover(entry.number)}
+              >
+                <div className="how__section__content__data__entry__step">
+                  {entry.step}
+                </div>
+
+                <div className="how__section__content__data__entry__dot">
+                  <div
+                    className={
+                      isMouseHover === entry.number
+                        ? "how__section__content__data__entry__dot__overlay__hover"
+                        : "how__section__content__data__entry__dot__overlay"
+                    }
+                  >
+                    <div
+                      className={
+                        isMouseHover === entry.number
+                          ? "how__section__content__data__entry__dot__overlay__point__hover"
+                          : "how__section__content__data__entry__dot__overlay__point"
+                      }
+                    ></div>
+                  </div>
+                </div>
+                <div className="how__section__content__data__entry__line"></div>
+                <div className="how__section__content__data__entry__number">
+                  {entry.number}
+                </div>
+                <div className="how__section__content__data__entry__heading">
+                  {entry.heading}
+                </div>
+                <div className="how__section__content__data__entry__text">
+                  {entry.text}
+                </div>
+              </div>
+            ))}
           </div>
         </ScrollContainer>
       </div>
