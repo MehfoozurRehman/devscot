70d5e71226e5fce8ffe96c941a28d74420a9a0ee
Arslan Goursi <arsljiger431@gmail.com>
2024-02-27 15:24:20 +0500

    animations

--- src/components/NumbersSection.jsx
+++ src/components/NumbersSection.jsx
@@ -1,23 +1,29 @@
 import React, { useState } from "react";
 
+import Fade from "react-reveal/Fade";
+
 export default function NumbersSection() {
   const [isMouseHover, setIsMouseHover] = useState(2);
   const entries = [
     {
       number: "25K+",
       text: "Project Completed",
+      delay: "100",
     },
     {
       number: "8K+",
       text: "Happy Costomers",
+      delay: "400",
     },
     {
       number: "25K+",
       text: "Years Experiences",
+      delay: "600",
     },
     {
       number: "25K+",
       text: "Awards Achievement",
+      delay: "800",
     },
   ];
 
@@ -25,28 +31,30 @@ export default function NumbersSection() {
     <div className="numbers__section">
       <div className="numbers__section__content">
         {entries.map((entry, index) => (
-          <div
-            className={
-              isMouseHover === index
-                ? "numbers__section__content__entry__hover"
-                : "numbers__section__content__entry"
-            }
-            key={index}
-            onMouseEnter={() => setIsMouseHover(index)}
-          >
-            <div className="numbers__section__content__entry__number">
-              {entry.number}
-            </div>
+          <Fade up delay={entry.delay}>
             <div
               className={
                 isMouseHover === index
-                  ? "numbers__section__content__entry__text__hover"
-                  : "numbers__section__content__entry__text"
+                  ? "numbers__section__content__entry__hover"
+                  : "numbers__section__content__entry"
               }
+              key={index}
+              onMouseEnter={() => setIsMouseHover(index)}
             >
-              {entry.text}
+              <div className="numbers__section__content__entry__number">
+                {entry.number}
+              </div>
+              <div
+                className={
+                  isMouseHover === index
+                    ? "numbers__section__content__entry__text__hover"
+                    : "numbers__section__content__entry__text"
+                }
+              >
+                {entry.text}
+              </div>
             </div>
-          </div>
+          </Fade>
         ))}
       </div>
     </div>
