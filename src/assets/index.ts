9022c7644c90569d690bb6aaa397b8da102f05f4
M Abrar Khalid <100939524+abrarkhalidofficial@users.noreply.github.com>
2024-02-28 12:37:03 +0500

    Update index.ts

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,6 +1,7 @@
 import PngDevscot1 from './devscot1.png'
 import PngDevscot2 from './devscot2.png'
 import PngFeatureSectionimg from './FeatureSectionimg.png'
+import JpgFooter from './footer.jpg'
 import PngHeaderlogo from './headerlogo.png'
 import PngHerobackground from './herobackground.png'
 
@@ -8,6 +9,7 @@ export {
   PngDevscot1,
   PngDevscot2,
   PngFeatureSectionimg,
+  JpgFooter,
   PngHeaderlogo,
   PngHerobackground
 }
