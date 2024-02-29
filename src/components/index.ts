f9b70382f14e024ba2d9bc4944d31a786d74ae7f
M Abrar Khalid <100939524+abrarkhalidofficial@users.noreply.github.com>
2024-02-29 15:13:13 +0500

    Update index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,10 @@
 import FeatureSection from './FeatureSection.jsx'
 import FeatureSectionMobile from './FeatureSectionMobile.jsx'
 import Footer from './Footer.jsx'
+import GetDemo from './GetDemo.jsx'
 import Header from './Header.jsx'
 import HowWorkSection from './HowWorkSection.jsx'
+import MealAbouttext from './MealAbouttext.jsx'
 import NumbersSection from './NumbersSection.jsx'
 import SolutionSection from './SolutionSection.jsx'
 
@@ -10,8 +12,10 @@ export {
   FeatureSection,
   FeatureSectionMobile,
   Footer,
+  GetDemo,
   Header,
   HowWorkSection,
+  MealAbouttext,
   NumbersSection,
   SolutionSection
 }
