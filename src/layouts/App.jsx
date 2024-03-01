4c08d2289750c81511ceda32df7618e47aa28acb
mehfoozurrehman <mehfoozijaz786@gmail.com>
2024-03-01 11:39:20 +0500

    Update App.jsx

--- src/layouts/App.jsx
+++ src/layouts/App.jsx
@@ -1,16 +1,5 @@
-import { Head } from "@router";
 import { Outlet } from "react-router-dom";
 
 export default () => {
-  return (
-    <>
-      <Head
-        title="Devscot"
-        image="/favicon.ico"
-        url="https://vitefilerouter.com"
-        description="Devscot"
-      />
-      <Outlet />
-    </>
-  );
+  return <Outlet />;
 };
