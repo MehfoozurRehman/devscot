f9a81700749bc9d68756766b409add36e09240ab
mehfoozurrehman <mehfoozijaz786@gmail.com>
2024-03-04 15:15:19 +0500

    Update HomeSection.jsx

--- src/components/HomeSection.jsx
+++ src/components/HomeSection.jsx
@@ -1,34 +1,67 @@
+"use client";
+
 import HomeSectionDown from "./HomeSectionDown";
+import { motion } from "framer-motion";
 import splitText from "../utils/splitText";
 
 export default function HomeSection() {
   const heading = splitText("Design. Development & Identity for your Product");
 
-  const info = splitText(
-    "We create modern user experience for new startups by creating a bridge between users’ needs and the client’s requirements. We expertise in developing"
-  );
-
   return (
     <section className="hero__section">
       <div className="hero__section__content">
-        <div className="hero__section__content__subheading">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1.5, delay: 0.1 }}
+          className="hero__section__content__subheading"
+        >
           We are heroes of
-        </div>
+        </motion.div>
         <div className="hero__section__content__heading">{heading}</div>
-        <div className="hero__section__content__info">{info}</div>
+        <motion.div
+          className="hero__section__content__info"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1.5, delay: 0.1 }}
+        >
+          We create modern user experience for new startups by creating a bridge
+          between users’ needs and the client’s requirements. We expertise in
+          developing.
+        </motion.div>
         <div className="hero__section__content__info__highlight">
-          <button className="hero__section__content__info__highlight__name">
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1.5, delay: 0.1 }}
+            className="hero__section__content__info__highlight__name"
+          >
             Mobile Apps
-          </button>
-          <button className="hero__section__content__info__highlight__name">
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1.5, delay: 0.1 }}
+            className="hero__section__content__info__highlight__name"
+          >
             Web Apps
-          </button>
-          <div className="hero__section__content__info__highlight__names">
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1.5, delay: 0.1 }}
+            className="hero__section__content__info__highlight__names"
+          >
             And
-          </div>
-          <button className="hero__section__content__info__highlight__name">
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1.5, delay: 0.1 }}
+            className="hero__section__content__info__highlight__name"
+          >
             degital Product Design
-          </button>
+          </motion.div>
         </div>
       </div>
       <img
