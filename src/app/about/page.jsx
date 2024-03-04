8c1b6ac8542ba35c61d0a23430cbd6ebf677c603
M Abrar Khalid <100939524+abrarkhalidofficial@users.noreply.github.com>
2024-03-04 17:23:55 +0500

    Create page.jsx

---
+++ src/app/about/page.jsx
@@ -0,0 +1,219 @@
+import { Header } from "@/components";
+import React from "react";
+
+export default function page() {
+  return (
+    <>
+      {/* <Header /> */}
+      <div className="Aboutuspage">
+        <div className="Aboutuspage__heading">
+          We’re a­ full­ service­ creative­ and­ digital­ agency,­ working­
+          globally­ with­ leargest ­brands
+        </div>
+        <div className="Aboutuspage__about">
+          <button className="Aboutuspage__about__button">
+            Trend & Technology
+          </button>
+          <div className="Aboutuspage__about__info">
+            hink of the world's most iconica and successful brands easily
+            findable, impactful. Interactional and exceptional designs. Our
+            story takes beginning in 2013
+          </div>
+          <img
+            src="
+          https://wealcoder.com/dev/html/axtra/assets/imgs/about/award.png"
+            alt=""
+            className="Aboutuspage__about__logo"
+          />
+        </div>
+        <div className="Aboutuspage__video">
+          <video
+            loop={true}
+            autoPlay={true}
+            muted={true}
+            playsInline={true}
+            src="https://wealcoder.com/dev/html/axtra/assets/video/video.mp4"
+            className="Aboutuspage__video__video"
+          ></video>
+        </div>
+        <div className="Aboutuspage__ourstory">
+          <div className="Aboutuspage__ourstory__content">
+            <div className="Aboutuspage__ourstory__content__left">
+              <div className="Aboutuspage__ourstory__content__subheading">
+                Digital Studio
+              </div>
+              <div className="Aboutuspage__ourstory__content__heading">
+                Our Story
+              </div>{" "}
+            </div>
+            <div className="Aboutuspage__ourstory__content__right">
+              <div className="Aboutuspage__ourstory__content__right__info">
+                Your brand is the most important asset in your company let our
+                team of professionals help you with a good strategy took the
+                runway next with an edgy collection featuring dyed denim pieces.
+                The collection included patchwork denim, a trend that has
+                recently exploded in younger generations. Playing on aspects of
+                sustainability, the pieces appeared to be upcycled to establish
+                dimension and flair. This take on grunge and streetwear took
+                sustainable fashion to an entirely new level. <br />
+                <br />
+                ur specialized team of researchers, strategists, designers,
+                developers, and project managers work with streamlined processes
+                to break through organizational roadblocks. We translate
+                research into solutions, crafting thoughtful and unified brands.
+              </div>
+            </div>
+          </div>
+          <div className="Aboutuspage__ourstory__content">
+            <div className="Aboutuspage__ourstory__content__images__left">
+              <img
+                src="	https://wealcoder.com/dev/html/axtra/assets/imgs/story/1.jpg"
+                alt=""
+                className="Aboutuspage__ourstory__content__images__left__image"
+              />
+            </div>
+            <div className="Aboutuspage__ourstory__content__images__right">
+              <img
+                src="	https://wealcoder.com/dev/html/axtra/assets/imgs/story/2.jpg"
+                alt=""
+                className="Aboutuspage__ourstory__content__images__right__image"
+              />
+            </div>
+            <div className="Aboutuspage__ourstory__content__images__rightlast">
+              <img
+                src="	https://wealcoder.com/dev/html/axtra/assets/imgs/story/3.jpg"
+                alt=""
+                className="Aboutuspage__ourstory__content__images__rightlast__image"
+              />
+              <img
+                src="	https://wealcoder.com/dev/html/axtra/assets/imgs/story/3.jpg"
+                alt=""
+                className="Aboutuspage__ourstory__content__images__rightlast__imagetwo"
+              />
+            </div>
+          </div>
+        </div>{" "}
+        <div className="Aboutuspage__numbers">
+          <div className="Aboutuspage__numbers__content">
+            <div className="Aboutuspage__numbers__content__card">
+              <div className="Aboutuspage__numbers__content__card__number">
+                10k
+              </div>
+              <div className="Aboutuspage__numbers__content__card__info">
+                Projects Completed
+              </div>
+            </div>
+            <div className="Aboutuspage__numbers__content__card">
+              <div className="Aboutuspage__numbers__content__card__number">
+                18k
+              </div>
+              <div className="Aboutuspage__numbers__content__card__info">
+                Happy customers
+              </div>
+            </div>
+            <div className="Aboutuspage__numbers__content__card">
+              <div className="Aboutuspage__numbers__content__card__number">
+                122
+              </div>
+              <div className="Aboutuspage__numbers__content__card__info">
+                Years experiences
+              </div>
+            </div>
+            <div className="Aboutuspage__numbers__content__card">
+              <div className="Aboutuspage__numbers__content__card__number">
+                132
+              </div>
+              <div className="Aboutuspage__numbers__content__card__info">
+                Awards achievement
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="Aboutuspage__logos">
+          <div className="Aboutuspage__logos__content">
+            <div className="Aboutuspage__logos__content__subheading">
+              Internation Brands
+            </div>
+            <div className="Aboutuspage__logos__content__heading">
+              We are happy to work with global largest brands
+            </div>
+            <div className="Aboutuspage__logos__content__logos">
+              <div className="Aboutuspage__logos__content__logos__card">
+                <img
+                  src="https://wealcoder.com/dev/html/axtra/assets/imgs/brand/1.png"
+                  alt=""
+                  className="Aboutuspage__logos__content__logos__card__image"
+                />
+              </div>
+
+              <div className="Aboutuspage__logos__content__logos__card">
+                <img
+                  src="https://wealcoder.com/dev/html/axtra/assets/imgs/brand/1.png"
+                  alt=""
+                  className="Aboutuspage__logos__content__logos__card__image"
+                />
+              </div>
+
+              <div className="Aboutuspage__logos__content__logos__card">
+                <img
+                  src="https://wealcoder.com/dev/html/axtra/assets/imgs/brand/1.png"
+                  alt=""
+                  className="Aboutuspage__logos__content__logos__card__image"
+                />
+              </div>
+              <div className="Aboutuspage__logos__content__logos__card">
+                <img
+                  src="https://wealcoder.com/dev/html/axtra/assets/imgs/brand/1.png"
+                  alt=""
+                  className="Aboutuspage__logos__content__logos__card__image"
+                />
+              </div>
+              <div className="Aboutuspage__logos__content__logos__card">
+                <img
+                  src="https://wealcoder.com/dev/html/axtra/assets/imgs/brand/1.png"
+                  alt=""
+                  className="Aboutuspage__logos__content__logos__card__image"
+                />
+              </div>
+              <div className="Aboutuspage__logos__content__logos__card">
+                <img
+                  src="https://wealcoder.com/dev/html/axtra/assets/imgs/brand/1.png"
+                  alt=""
+                  className="Aboutuspage__logos__content__logos__card__image"
+                />
+              </div>
+              <div className="Aboutuspage__logos__content__logos__card">
+                <img
+                  src="https://wealcoder.com/dev/html/axtra/assets/imgs/brand/1.png"
+                  alt=""
+                  className="Aboutuspage__logos__content__logos__card__image"
+                />
+              </div>
+              <div className="Aboutuspage__logos__content__logos__card">
+                <img
+                  src="https://wealcoder.com/dev/html/axtra/assets/imgs/brand/1.png"
+                  alt=""
+                  className="Aboutuspage__logos__content__logos__card__image"
+                />
+              </div>
+              <div className="Aboutuspage__logos__content__logos__card">
+                <img
+                  src="https://wealcoder.com/dev/html/axtra/assets/imgs/brand/1.png"
+                  alt=""
+                  className="Aboutuspage__logos__content__logos__card__image"
+                />
+              </div>
+              <div className="Aboutuspage__logos__content__logos__card">
+                <img
+                  src="https://wealcoder.com/dev/html/axtra/assets/imgs/brand/1.png"
+                  alt=""
+                  className="Aboutuspage__logos__content__logos__card__image"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
