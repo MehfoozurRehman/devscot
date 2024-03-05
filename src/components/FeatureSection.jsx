96d972e498ab2fa86e710a057ba2ee5f9c1dd62f
mehfoozurrehman <mehfoozijaz786@gmail.com>
2024-03-05 10:17:38 +0500

    Update FeatureSection.jsx

--- src/components/FeatureSection.jsx
+++ src/components/FeatureSection.jsx
@@ -1,5 +1,4 @@
-import { ArrowUpRight } from "react-feather";
-import Link from "next/link";
+import FeatureSectionCard from "./FeatureSectionCard";
 
 export default function FeatureSection() {
   return (
@@ -26,38 +25,12 @@ export default function FeatureSection() {
             </div>
           </div>
           <div className="FeatureSection__content__right">
-            <Card />
-            <Card />
-            <Card />
+            <FeatureSectionCard />
+            <FeatureSectionCard />
+            <FeatureSectionCard />
           </div>
         </div>
       </div>
-    </div>
-  );
-}
-
-function Card({}) {
-  return (
-    <div className="FeatureSection__content__right__content">
-      <img
-        src="/FeatureSectionimg.png"
-        alt=""
-        className="FeatureSection__content__right__img"
-      />
-      <Link
-        href="/Projects/Meal"
-        className="FeatureSection__content__right__content__card"
-      >
-        <div className="FeatureSection__content__right__content__card__heading">
-          Project Name
-          <div className="FeatureSection__content__right__content__card__arrow">
-            <ArrowUpRight />
-          </div>
-        </div>
-        <div className="FeatureSection__content__right__content__card__info">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        </div>
-      </Link>
     </div>
   );
 }
