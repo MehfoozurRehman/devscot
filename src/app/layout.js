38a2b64241eba2f5c275559630f9582393d6515d
mehfoozurrehman <mehfoozijaz786@gmail.com>
2024-03-05 10:16:14 +0500

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import "@/styles/global.scss";
 
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata = {
@@ -11,7 +13,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <Header />
         {children}
+        <Footer />
         <SpeedInsights />
       </body>
     </html>
