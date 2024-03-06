7763d0377c517477374f3af3399c33df003f6a3f
mehfoozurrehman <mehfoozijaz786@gmail.com>
2024-03-06 11:42:03 +0500

    Update FeatureSectionCard.jsx

--- src/components/FeatureSectionCard.jsx
+++ src/components/FeatureSectionCard.jsx
@@ -1,28 +1,37 @@
+"use client";
+
 import { ArrowUpRight } from "react-feather";
 import Link from "next/link";
+import { motion } from "framer-motion";
 
 export default function FeatureSectionCard() {
   return (
-    <div className="FeatureSection__content__right__content">
-      <img
-        src="/FeatureSectionimg.png"
-        alt=""
-        className="FeatureSection__content__right__img"
-      />
-      <Link
-        href="/Projects/Meal"
-        className="FeatureSection__content__right__content__card"
-      >
-        <div className="FeatureSection__content__right__content__card__heading">
-          Project Name
-          <div className="FeatureSection__content__right__content__card__arrow">
-            <ArrowUpRight />
+    <Link href="/">
+      <div className="FeatureSection__content__right__content">
+        <motion.img
+          src="/FeatureSectionimg.png"
+          className="FeatureSection__content__right__img"
+          initial={{ scale: 1.5, y: -100 }}
+          whileInView={{ scale: 1, y: 0 }}
+          transition={{ duration: 1, type: "tween" }}
+        />
+        <motion.div
+          initial={{ x: 100, scale: 0.9 }}
+          whileInView={{ x: 0, scale: 1 }}
+          transition={{ duration: 1 }}
+          className="FeatureSection__content__right__content__card"
+        >
+          <div className="FeatureSection__content__right__content__card__heading">
+            Project Name
+            <div className="FeatureSection__content__right__content__card__arrow">
+              <ArrowUpRight />
+            </div>
           </div>
-        </div>
-        <div className="FeatureSection__content__right__content__card__info">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        </div>
-      </Link>
-    </div>
+          <div className="FeatureSection__content__right__content__card__info">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </div>
+        </motion.div>
+      </div>
+    </Link>
   );
 }
