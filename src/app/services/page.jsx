d231b7ce1c40d806c3f58c5b2418e0b1f5ce33b4
M Abrar Khalid <100939524+abrarkhalidofficial@users.noreply.github.com>
2024-03-18 10:34:52 +0500

    Create page.jsx

---
+++ src/app/services/page.jsx
@@ -0,0 +1,148 @@
+import React from "react";
+import "@/styles/serivces.scss";
+export default function page() {
+  return (
+    <>
+      <div className="Services__deatils">
+        <div className="Services__deatils__all__content">
+          <div className="Services__deatils__content">
+            <div className="Services__deatils__content__left">
+              <div className="Services__deatils__content__left__conent">
+                <div className="Services__deatils__content__left__conent__heading">
+                  Web and mobile development
+                </div>
+              </div>
+            </div>
+            <div className="Services__deatils__content__right">
+              <div className="Services__deatils__content__right__content">
+                <div className="Services__deatils__content__right__content__info">
+                  Every decision we make needs to answer the million-dollar
+                  question: how will this benefit our partner’s goal best? We
+                  work to develop solutions
+                </div>
+                <div className="Services__deatils__content__right__content__info">
+                  Every decision we make needs to answer the million-dollar
+                  question: how will this benefit our partner’s goal best? We
+                  work to develop solutions
+                </div>
+              </div>
+              <div className="Services__deatils__content__right__contentbottom">
+                <div className="Services__deatils__content__right__contentbottom__content">
+                  <div className="Services__deatils__content__right__contentbottom__points">
+                    + API Development
+                  </div>
+                  <div className="Services__deatils__content__right__contentbottom__points">
+                    + Cloud Migration
+                  </div>
+                  <div className="Services__deatils__content__right__contentbottom__points">
+                    + JavaScript
+                  </div>{" "}
+                </div>
+                <div className="Services__deatils__content__right__contentbottom__content">
+                  <div className="Services__deatils__content__right__contentbottom__points">
+                    + WordPress
+                  </div>
+                  <div className="Services__deatils__content__right__contentbottom__points">
+                    + Front End Development
+                  </div>
+                  <div className="Services__deatils__content__right__contentbottom__points">
+                    + Fluter Framework
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>{" "}
+          <div className="Services__deatils__content__right__contentbottom__imgs">
+            <img
+              className="Services__deatils__content__right__contentbottom__imgs__img"
+              src="https://wealcoder.com/dev/html/axtra/assets/imgs/thumb/dev-1.jpg"
+              alt=""
+            />
+            <img
+              className="Services__deatils__content__right__contentbottom__imgs__img__two"
+              src="https://wealcoder.com/dev/html/axtra/assets/imgs/thumb/dev-2.jpg"
+              alt=""
+            />
+          </div>{" "}
+        </div>
+      </div>
+      <div className="Services__deatils__card">
+        <div className="Services__deatils__card__content">
+          <div className="Services__deatils__card__content__cards">
+            <div className="Services__deatils__card__content__cards__card">
+              <div className="Services__deatils__card__content__cards__card__heading">
+                Planning and sketching
+              </div>
+              <div className="Services__deatils__card__content__cards__card__info">
+                Modern and unique design practically point of view, it risks not
+                meeting the huge expectations
+              </div>
+            </div>
+            <div className="Services__deatils__card__content__cards__card">
+              <div className="Services__deatils__card__content__cards__card__heading">
+                Planning and sketching
+              </div>
+              <div className="Services__deatils__card__content__cards__card__info">
+                Modern and unique design practically point of view, it risks not
+                meeting the huge expectations
+              </div>
+            </div>
+            <div className="Services__deatils__card__content__cards__card">
+              <div className="Services__deatils__card__content__cards__card__heading">
+                Planning and sketching
+              </div>
+              <div className="Services__deatils__card__content__cards__card__info">
+                Modern and unique design practically point of view, it risks not
+                meeting the huge expectations
+              </div>
+            </div>
+            <div className="Services__deatils__card__content__cards__card">
+              <div className="Services__deatils__card__content__cards__card__heading">
+                Planning and sketching
+              </div>
+              <div className="Services__deatils__card__content__cards__card__info">
+                Modern and unique design practically point of view, it risks not
+                meeting the huge expectations
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="Services__deatils__layer">
+        <div className="Services__deatils__layer__content__heading">
+          We replace unreliable wirefreme and expensive agencies for one of the
+          best organized layer.
+        </div>
+        <div className="Services__deatils__layer__content">
+          <div className="Services__deatils__layer__content__left">
+            <span className="Services__deatils__layer__content__left__span"></span>
+          </div>
+          <div className="Services__deatils__layer__content__right">
+            <div className="Services__deatils__layer__content__right__content">
+              <img
+                src="https://wealcoder.com/dev/html/axtra/assets/imgs/thumb/service-detail.png"
+                alt=""
+                className="Services__deatils__layer__content__right__content__img"
+              />
+              <div className="Services__deatils__layer__content__right__content__info">
+                <div className="Services__deatils__layer__content__right__content__info__info">
+                  Receive your design within a few business days, and be updated
+                  on the process. Everything you need for a digitally driven
+                  brand. Defined proposition. Conceptual realisation. Logo,
+                  type, look, feel, tone, movement, content - we've got it
+                  covered.
+                </div>
+                <div className="Services__deatils__layer__content__right__content__info__info">
+                  Getting your brand message out there. We create dynamic
+                  campaign creative that engages audiences, wherever they are
+                  most talented. Bring your brand to life, communicate your
+                  value proposition with agile setup across creativity.
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
