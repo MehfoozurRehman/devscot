727cc75fc3f1878f0b8f317aeff69f7c976f198f
M Abrar Khalid <100939524+abrarkhalidofficial@users.noreply.github.com>
2024-03-20 08:50:46 +0500

    Create ServicesHowWeDoIt.jsx

---
+++ src/components/ServicesHowWeDoIt.jsx
@@ -0,0 +1,47 @@
+import ServicesHowWeDoItEntry from "./ServicesHowWeDoItEntry";
+
+export default function ServicesHowWeDoIt() {
+  return (
+    <div className="services__how__we__do__it">
+      <div className="services__how__we__do__it__heading heading">
+        How we do it?
+      </div>
+      <div className="services__how__we__do__it__info">
+        Fast-track your project's delivery using our simple three-step
+        application development process.
+      </div>
+      <div className="services__how__we__do__it__content">
+        <ServicesHowWeDoItEntry
+          icon="1"
+          title="Requirements Gathering"
+          list={[
+            "Requirements Definition",
+            "Analyzing the requirements",
+            "Documenting the process",
+            "Provide initial design",
+          ]}
+        />
+        <ServicesHowWeDoItEntry
+          icon="2"
+          title="Development & Testing"
+          list={[
+            "Frontend & backend development",
+            "Weekly Client Feedback (SCRUM)",
+            "QA Testing",
+            "Deployment",
+          ]}
+        />
+        <ServicesHowWeDoItEntry
+          icon="3"
+          title="Support & Maintenance"
+          list={[
+            "SLA Based Support",
+            "L3 and Production Support Services",
+            "Operational support where needed",
+            "On-going Support",
+          ]}
+        />
+      </div>
+    </div>
+  );
+}
