72e3ef6d2a7f289ce52ffd2ca10642813388fd58
M Abrar Khalid <100939524+abrarkhalidofficial@users.noreply.github.com>
2024-03-22 12:50:37 +0500

    Update FeatureSection.jsx

--- src/components/FeatureSection.jsx
+++ src/components/FeatureSection.jsx
@@ -1,3 +1,5 @@
+"use client";
+import { Fade } from "react-reveal";
 import FeatureSectionCard from "./FeatureSectionCard";
 
 export default function FeatureSection() {
@@ -7,22 +9,32 @@ export default function FeatureSection() {
         <div className="FeatureSection__content">
           <div className="FeatureSection__content__left">
             <div className="FeatureSection__content__left__content">
-              <div className="FeatureSection__content__left__content__headingsub">
-                FEATURED <br />
-              </div>
-              <div className="FeatureSection__content__left__content__heading">
-                Projects
-              </div>
-              <div className="FeatureSection__content__left__content__info">
-                View the full case study of our recent featured and awesome
-                works that we created for our clients.
-              </div>
+              <Fade up delay={100}>
+                <div className="FeatureSection__content__left__content__headingsub">
+                  FEATURED <br />
+                </div>
+              </Fade>
+              <Fade up delay={100}>
+                {" "}
+                <div className="FeatureSection__content__left__content__heading">
+                  Projects
+                </div>
+              </Fade>
+              <Fade up delay={100}>
+                {" "}
+                <div className="FeatureSection__content__left__content__info">
+                  View the full case study of our recent featured and awesome
+                  works that we created for our clients.
+                </div>{" "}
+              </Fade>
             </div>
-            <div className="FeatureSection__content__left__content__div">
-              <button className="FeatureSection__content__left__content__button">
-                View All Projects
-              </button>
-            </div>
+            <Fade up delay={100}>
+              <div className="FeatureSection__content__left__content__div">
+                <button className="FeatureSection__content__left__content__button">
+                  View All Projects
+                </button>
+              </div>
+            </Fade>
           </div>
           <div className="FeatureSection__content__right">
             <FeatureSectionCard />
