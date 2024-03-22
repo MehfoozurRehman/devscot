2ccd75e69a7ac866124fb15dd7d6bd02c5269a6a
M Abrar Khalid <100939524+abrarkhalidofficial@users.noreply.github.com>
2024-03-22 11:57:03 +0500

    Update page.jsx

--- src/app/services/[servicesDetails]/page.jsx
+++ src/app/services/[servicesDetails]/page.jsx
@@ -1,236 +1,101 @@
 "use client";
 
-import "@/styles/servicesdetails.scss";
-import "@/styles/servicescard.scss";
+import "@/styles/serivces.scss";
 
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-import MagnetButton from "@/components/MagnetButton";
-import ScrollSpy from "react-ui-scrollspy";
+import HowWorkSection from "@/components/HowWorkSection";
+import React from "react";
+import ServicesHowWeDoIt from "@/components/ServicesHowWeDoIt";
 import { motion } from "framer-motion";
-import { useRef } from "react";
+import splitText from "@/utils/splitText";
 
-const Data = [
-  {
-    title: "Web Development",
-    description:
-      "We’re designing digital experiences that enrich human lives and it helps to grow your business globally trends.",
-    links: ["Approch", "Creativity", "Experienced"],
-    id: 1,
-    img: "/FeatureSectionimg.webp",
-  },
-  {
-    title: "Mobile App Development",
-    description:
-      "We’re designing digital experiences that enrich human lives and it helps to grow your business globally trends.",
-    links: ["Approch", "Creativity", "Experienced"],
-    id: 2,
-    img: "/FeatureSectionimg.webp",
-  },
-  {
-    title: "Digital Marketing",
-    description:
-      "We’re designing digital experiences that enrich human lives and it helps to grow your business globally trends.",
-    links: ["Approch", "Creativity", "Experienced"],
-    id: 3,
-    img: "/FeatureSectionimg.webp",
-  },
-  {
-    title: "UI/UX Design",
-    description:
-      "We’re designing digital experiences that enrich human lives and it helps to grow your business globally trends.",
-    links: ["Approch", "Creativity", "Experienced"],
-    id: 4,
-    img: "/FeatureSectionimg.webp",
-  },
-  {
-    title: "Branding",
-    description:
-      "We’re designing digital experiences that enrich human lives and it helps to grow your business globally trends.",
-    links: ["Approch", "Creativity", "Experienced"],
-    id: 5,
-    img: "/FeatureSectionimg.webp",
-  },
-  {
-    title: "E-commerce",
-    description:
-      "We’re designing digital experiences that enrich human lives and it helps to grow your business globally trends.",
-    links: ["Approch", "Creativity", "Experienced"],
-    id: 6,
-    img: "/FeatureSectionimg.webp",
-  },
-];
-
-function ServicePage() {
-  const parentScrollContainerRef = useRef(null);
-
+export default function page() {
+  const heading = splitText("Web and mobile development");
   return (
     <>
-      <div
-        style={{
-          scrollSnapType: "y mandatory",
-          overflowY: "scroll",
-          height: "100vh",
-          marginTop: "50px",
-        }}
-        className="services__details__page"
-        ref={parentScrollContainerRef}
-      >
-        <Header />
-        <div id="smooth-wrapper" style={{ scrollSnapAlign: "start" }}>
-          <div id="smooth-content">
-            <main>
-              <section className="solution__area">
-                <div className="container hero-line" />
-                <div className="solution__wrapper">
-                  <div className="solution__left">
-                    <div className="solution__img-1">
-                      <img
-                        src="https://wealcoder.com/dev/html/axtra/assets/imgs/thumb/solution.png"
-                        alt="Solution Image"
-                      />
-                    </div>
-                    <div className="solution__img-2">
-                      <img
-                        src="https://wealcoder.com/dev/html/axtra/assets/imgs/thumb/solution-2.png"
-                        alt="Solution Image"
-                      />
-                    </div>
-                  </div>
-                  <div className="solution__mid">
-                    <h1 className="solution__title animation__char_come">
-                      Digital Solution
-                    </h1>
-                    <p>
-                      We’re designing digital experiences that enrich human
-                      lives and it helps to grow your business globally trends.
-                    </p>
-                  </div>
-                  <div className="solution__right">
-                    <div className="solution__img-3">
-                      <img
-                        src="https://wealcoder.com/dev/html/axtra/assets/imgs/thumb/solution-3.png"
-                        alt="Solution Image"
-                      />
-                    </div>
-                  </div>
-                </div>
-                <div className="container pb-130">
-                  <div className="row">
-                    <div className="col-xxl-12">
-                      <div className="solution__btm">
-                        <ul>
-                          <li>Approch</li>
-                          <li>Creativity</li>
-                          <li>Experienced</li>
-                        </ul>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="solution__shape">
-                  <img
-                    src="https://wealcoder.com/dev/html/axtra/assets/imgs/icon/1.png"
-                    alt="Shape Image"
-                    className="shape-1"
-                  />
-                  <img
-                    src="https://wealcoder.com/dev/html/axtra/assets/imgs/icon/2.png"
-                    alt="Shape Image"
-                    className="shape-2"
-                  />
-                  <img
-                    src="https://wealcoder.com/dev/html/axtra/assets/imgs/icon/3.png"
-                    alt="Shape Image"
-                    className="shape-3"
-                  />
-                  <img
-                    src="https://wealcoder.com/dev/html/axtra/assets/imgs/icon/4.png"
-                    alt="Shape Image"
-                    className="shape-4"
-                  />
-                  <img
-                    src="https://wealcoder.com/dev/html/axtra/assets/imgs/icon/5.png"
-                    alt="Shape Image"
-                    className="shape-5"
-                  />
-                </div>
-              </section>
-            </main>
-          </div>
-        </div>
-        <div className="service__Pagedetails__section">
-          <div className="service__Pagedetails__section__content">
-            <div className="service__Pagedetails__section__content__left">
-              {Data.map((item) => {
-                return (
-                  <div
-                    className="service__Pagedetails__section__content__left__links"
-                    key={item.id}
-                  >
-                    <div
-                      href={`#${item.id}`}
-                      className="service__Pagedetails__section__content__left__links__link "
-                    >
-                      <div data-to-scrollspy-id={item.id}>{item.title}</div>
-                    </div>
-                  </div>
-                );
-              })}
+      <Header />
+      <div className="Services__deatils">
+        <div className="Services__deatils__all__content">
+          <div className="Services__deatils__content">
+            <div className="Services__deatils__content__left">
+              <div className="Services__deatils__content__left__conent">
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 1.5 }}
+                  className="Services__deatils__content__left__conent__heading"
+                >
+                  {heading}
+                </motion.div>
+              </div>
             </div>
-            <div className="service__Pagedetails__section__content__right">
-              <ScrollSpy
-                scrollThrottle={10}
-                useBoxMethod={false}
-                offsetTop={-250}
-                parentScrollContainerRef={parentScrollContainerRef}
-              >
-                {Data?.map((item) => (
-                  <section
-                    className="service__Pagedetails__section__content__right__content__section"
-                    id={item.id}
-                    key={item.id}
-                  >
-                    <div className="service__Pagedetails__section__content__mid__imgs">
-                      <motion.img
-                        initial={{ scale: 1.5 }}
-                        whileInView={{ scale: 1 }}
-                        transition={{ duration: 1.5 }}
-                        src={item.img}
-                        alt={item.title}
-                      />
-                    </div>
-                    <div className="service__Pagedetails__section__content__right__content">
-                      <h2 className="service__Pagedetails__section__content__right__content__heading">
-                        {item.title}
-                      </h2>
-                      <p className="service__Pagedetails__section__content__right__content__info">
-                        {item.description}
-                      </p>
-                      <div className="service__Pagedetails__section__content__right__content__heading__links">
-                        <ul>
-                          <li>Approch</li>
-                          <li>Creativity</li>
-                          <li>Experienced</li>
-                        </ul>
-                        <ul>
-                          <li>Approch</li>
-                          <li>Creativity</li>
-                          <li>Experienced</li>
-                        </ul>
-                      </div>
-                      <MagnetButton>Get Started</MagnetButton>
-                    </div>
-                  </section>
-                ))}
-              </ScrollSpy>
+            <div className="Services__deatils__content__right">
+              <div className="Services__deatils__content__right__content">
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 1.5 }}
+                  className="Services__deatils__content__right__content__info"
+                >
+                  Every decision we make needs to answer the million-dollar
+                  question: how will this benefit our partner’s goal best? We
+                  work to develop solutions
+                </motion.div>
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 1.5 }}
+                  className="Services__deatils__content__right__content__info"
+                >
+                  Every decision we make needs to answer the million-dollar
+                  question: how will this benefit our partner’s goal best? We
+                  work to develop solutions
+                </motion.div>
+              </div>
+              <div className="Services__deatils__content__right__contentbottom">
+                <div className="Services__deatils__content__right__contentbottom__content">
+                  <div className="Services__deatils__content__right__contentbottom__points">
+                    + API Development
+                  </div>
+                  <div className="Services__deatils__content__right__contentbottom__points">
+                    + Cloud Migration
+                  </div>
+                  <div className="Services__deatils__content__right__contentbottom__points">
+                    + JavaScript
+                  </div>{" "}
+                </div>
+                <div className="Services__deatils__content__right__contentbottom__content">
+                  <div className="Services__deatils__content__right__contentbottom__points">
+                    + WordPress
+                  </div>
+                  <div className="Services__deatils__content__right__contentbottom__points">
+                    + Front End Development
+                  </div>
+                  <div className="Services__deatils__content__right__contentbottom__points">
+                    + Fluter Framework
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
+          </div>{" "}
+          <div className="Services__deatils__content__right__contentbottom__imgs">
+            <img
+              className="Services__deatils__content__right__contentbottom__imgs__img"
+              src="https://wealcoder.com/dev/html/axtra/assets/imgs/thumb/dev-1.png"
+              alt=""
+            />
+            <img
+              className="Services__deatils__content__right__contentbottom__imgs__img__two"
+              src="https://wealcoder.com/dev/html/axtra/assets/imgs/thumb/dev-2.png"
+              alt=""
+            />
+          </div>{" "}
         </div>
       </div>
+      <HowWorkSection />
+      <ServicesHowWeDoIt />
       <Footer />
     </>
   );
 }
-
-export default ServicePage;
