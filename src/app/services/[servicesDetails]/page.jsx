340121ff7c2294aec89ec04a01d81ec263d1ed7f
mehfoozurrehman <mehfoozijaz786@gmail.com>
2024-03-22 08:21:06 +0500

    Update page.jsx

--- src/app/services/[servicesDetails]/page.jsx
+++ src/app/services/[servicesDetails]/page.jsx
@@ -3,8 +3,12 @@
 import "@/styles/servicesdetails.scss";
 import "@/styles/servicescard.scss";
 
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
 import MagnetButton from "@/components/MagnetButton";
 import ScrollSpy from "react-ui-scrollspy";
+import { motion } from "framer-motion";
+import { useRef } from "react";
 
 const Data = [
   {
@@ -13,7 +17,7 @@ const Data = [
       "We’re designing digital experiences that enrich human lives and it helps to grow your business globally trends.",
     links: ["Approch", "Creativity", "Experienced"],
     id: 1,
-    img: "/FeatureSectionimg.png",
+    img: "/FeatureSectionimg.webp",
   },
   {
     title: "Mobile App Development",
@@ -21,7 +25,7 @@ const Data = [
       "We’re designing digital experiences that enrich human lives and it helps to grow your business globally trends.",
     links: ["Approch", "Creativity", "Experienced"],
     id: 2,
-    img: "/FeatureSectionimg.png",
+    img: "/FeatureSectionimg.webp",
   },
   {
     title: "Digital Marketing",
@@ -29,7 +33,7 @@ const Data = [
       "We’re designing digital experiences that enrich human lives and it helps to grow your business globally trends.",
     links: ["Approch", "Creativity", "Experienced"],
     id: 3,
-    img: "/FeatureSectionimg.png",
+    img: "/FeatureSectionimg.webp",
   },
   {
     title: "UI/UX Design",
@@ -37,7 +41,7 @@ const Data = [
       "We’re designing digital experiences that enrich human lives and it helps to grow your business globally trends.",
     links: ["Approch", "Creativity", "Experienced"],
     id: 4,
-    img: "/FeatureSectionimg.png",
+    img: "/FeatureSectionimg.webp",
   },
   {
     title: "Branding",
@@ -45,7 +49,7 @@ const Data = [
       "We’re designing digital experiences that enrich human lives and it helps to grow your business globally trends.",
     links: ["Approch", "Creativity", "Experienced"],
     id: 5,
-    img: "/FeatureSectionimg.png",
+    img: "/FeatureSectionimg.webp",
   },
   {
     title: "E-commerce",
@@ -53,189 +57,179 @@ const Data = [
       "We’re designing digital experiences that enrich human lives and it helps to grow your business globally trends.",
     links: ["Approch", "Creativity", "Experienced"],
     id: 6,
-    img: "/FeatureSectionimg.png",
+    img: "/FeatureSectionimg.webp",
   },
 ];
 
 function ServicePage() {
-  const scrollToSection = (e, offset = 0) => {
-    e.preventDefault();
-    const targetId = e.currentTarget.href.split("#")[1];
-    const targetElement = document.getElementById(targetId);
-    if (targetElement) {
-      let yOffset = 0;
-      if (window.innerWidth >= 1220) {
-        yOffset = -150;
-      } else if (window.innerWidth >= 950) {
-        yOffset = -195;
-      } else {
-        yOffset = -250;
-      }
-      const y =
-        targetElement.getBoundingClientRect().top +
-        window.scrollY +
-        yOffset -
-        offset;
-      window.scrollTo({ top: y, behavior: "smooth" });
-    }
-  };
+  const parentScrollContainerRef = useRef(null);
+
   return (
-    <div>
-      <div id="smooth-wrapper">
-        <div id="smooth-content">
-          <main>
-            <section className="solution__area">
-              <div className="container hero-line" />
-              <div className="solution__wrapper">
-                <div className="solution__left">
-                  <div className="solution__img-1">
-                    <img
-                      src="https://wealcoder.com/dev/html/axtra/assets/imgs/thumb/solution.png"
-                      alt="Solution Image"
-                    />
+    <>
+      <div
+        style={{
+          scrollSnapType: "y mandatory",
+          overflowY: "scroll",
+          height: "100vh",
+          marginTop: "50px",
+        }}
+        className="services__details__page"
+        ref={parentScrollContainerRef}
+      >
+        <Header />
+        <div id="smooth-wrapper" style={{ scrollSnapAlign: "start" }}>
+          <div id="smooth-content">
+            <main>
+              <section className="solution__area">
+                <div className="container hero-line" />
+                <div className="solution__wrapper">
+                  <div className="solution__left">
+                    <div className="solution__img-1">
+                      <img
+                        src="https://wealcoder.com/dev/html/axtra/assets/imgs/thumb/solution.png"
+                        alt="Solution Image"
+                      />
+                    </div>
+                    <div className="solution__img-2">
+                      <img
+                        src="https://wealcoder.com/dev/html/axtra/assets/imgs/thumb/solution-2.png"
+                        alt="Solution Image"
+                      />
+                    </div>
                   </div>
-                  <div className="solution__img-2">
-                    <img
-                      src="https://wealcoder.com/dev/html/axtra/assets/imgs/thumb/solution-2.png"
-                      alt="Solution Image"
-                    />
+                  <div className="solution__mid">
+                    <h1 className="solution__title animation__char_come">
+                      Digital Solution
+                    </h1>
+                    <p>
+                      We’re designing digital experiences that enrich human
+                      lives and it helps to grow your business globally trends.
+                    </p>
                   </div>
-                </div>
-                <div className="solution__mid">
-                  <h1 className="solution__title animation__char_come">
-                    Digital Solution
-                  </h1>
-                  <p>
-                    We’re designing digital experiences that enrich human lives
-                    and it helps to grow your business globally trends.
-                  </p>
-                </div>
-                <div className="solution__right">
-                  <div className="solution__img-3">
-                    <img
-                      src="https://wealcoder.com/dev/html/axtra/assets/imgs/thumb/solution-3.png"
-                      alt="Solution Image"
-                    />
-                  </div>
-                </div>
-              </div>
-              <div className="container pb-130">
-                <div className="row">
-                  <div className="col-xxl-12">
-                    <div className="solution__btm">
-                      <ul>
-                        <li>Approch</li>
-                        <li>Creativity</li>
-                        <li>Experienced</li>
-                      </ul>
+                  <div className="solution__right">
+                    <div className="solution__img-3">
+                      <img
+                        src="https://wealcoder.com/dev/html/axtra/assets/imgs/thumb/solution-3.png"
+                        alt="Solution Image"
+                      />
                     </div>
                   </div>
                 </div>
-              </div>
-              <div className="solution__shape">
-                <img
-                  src="https://wealcoder.com/dev/html/axtra/assets/imgs/icon/1.png"
-                  alt="Shape Image"
-                  className="shape-1"
-                />
-                <img
-                  src="https://wealcoder.com/dev/html/axtra/assets/imgs/icon/2.png"
-                  alt="Shape Image"
-                  className="shape-2"
-                />
-                <img
-                  src="https://wealcoder.com/dev/html/axtra/assets/imgs/icon/3.png"
-                  alt="Shape Image"
-                  className="shape-3"
-                />
-                <img
-                  src="https://wealcoder.com/dev/html/axtra/assets/imgs/icon/4.png"
-                  alt="Shape Image"
-                  className="shape-4"
-                />
-                <img
-                  src="https://wealcoder.com/dev/html/axtra/assets/imgs/icon/5.png"
-                  alt="Shape Image"
-                  className="shape-5"
-                />
-              </div>
-            </section>
-          </main>
-        </div>
-      </div>
-      <div className="service__Pagedetails__section">
-        <div className="service__Pagedetails__section__content">
-          <div className="service__Pagedetails__section__content__left">
-            {Data.map((item) => {
-              return (
-                <div className="service__Pagedetails__section__content__left__links">
-                  <div
-                    onClick={(e) => scrollToSection(e)}
-                    href={`#${item.id}`}
-                    className="service__Pagedetails__section__content__left__links__link "
-                  >
-                    <div data-to-scrollspy-id={item.id}>{item.title}</div>
+                <div className="container pb-130">
+                  <div className="row">
+                    <div className="col-xxl-12">
+                      <div className="solution__btm">
+                        <ul>
+                          <li>Approch</li>
+                          <li>Creativity</li>
+                          <li>Experienced</li>
+                        </ul>
+                      </div>
+                    </div>
                   </div>
                 </div>
-              );
-            })}{" "}
-          </div>{" "}
-          <div className="service__Pagedetails__section__content__mid">
-            <div className="service__Pagedetails__section__content__mid__imgs">
+                <div className="solution__shape">
+                  <img
+                    src="https://wealcoder.com/dev/html/axtra/assets/imgs/icon/1.png"
+                    alt="Shape Image"
+                    className="shape-1"
+                  />
+                  <img
+                    src="https://wealcoder.com/dev/html/axtra/assets/imgs/icon/2.png"
+                    alt="Shape Image"
+                    className="shape-2"
+                  />
+                  <img
+                    src="https://wealcoder.com/dev/html/axtra/assets/imgs/icon/3.png"
+                    alt="Shape Image"
+                    className="shape-3"
+                  />
+                  <img
+                    src="https://wealcoder.com/dev/html/axtra/assets/imgs/icon/4.png"
+                    alt="Shape Image"
+                    className="shape-4"
+                  />
+                  <img
+                    src="https://wealcoder.com/dev/html/axtra/assets/imgs/icon/5.png"
+                    alt="Shape Image"
+                    className="shape-5"
+                  />
+                </div>
+              </section>
+            </main>
+          </div>
+        </div>
+        <div className="service__Pagedetails__section">
+          <div className="service__Pagedetails__section__content">
+            <div className="service__Pagedetails__section__content__left">
+              {Data.map((item) => {
+                return (
+                  <div
+                    className="service__Pagedetails__section__content__left__links"
+                    key={item.id}
+                  >
+                    <div
+                      href={`#${item.id}`}
+                      className="service__Pagedetails__section__content__left__links__link "
+                    >
+                      <div data-to-scrollspy-id={item.id}>{item.title}</div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+            <div className="service__Pagedetails__section__content__right">
               <ScrollSpy
                 scrollThrottle={10}
                 useBoxMethod={false}
                 offsetTop={-250}
+                parentScrollContainerRef={parentScrollContainerRef}
               >
-                {" "}
-                {Data.map((item) => (
+                {Data?.map((item) => (
                   <section
+                    className="service__Pagedetails__section__content__right__content__section"
                     id={item.id}
-                    className="service__Pagedetails__section__content__mid__imgs__img"
+                    key={item.id}
                   >
-                    <img src="/FeatureSectionimg.webp" alt="Solution Image" />
+                    <div className="service__Pagedetails__section__content__mid__imgs">
+                      <motion.img
+                        initial={{ scale: 1.5 }}
+                        whileInView={{ scale: 1 }}
+                        transition={{ duration: 1.5 }}
+                        src={item.img}
+                        alt={item.title}
+                      />
+                    </div>
+                    <div className="service__Pagedetails__section__content__right__content">
+                      <h2 className="service__Pagedetails__section__content__right__content__heading">
+                        {item.title}
+                      </h2>
+                      <p className="service__Pagedetails__section__content__right__content__info">
+                        {item.description}
+                      </p>
+                      <div className="service__Pagedetails__section__content__right__content__heading__links">
+                        <ul>
+                          <li>Approch</li>
+                          <li>Creativity</li>
+                          <li>Experienced</li>
+                        </ul>
+                        <ul>
+                          <li>Approch</li>
+                          <li>Creativity</li>
+                          <li>Experienced</li>
+                        </ul>
+                      </div>
+                      <MagnetButton>Get Started</MagnetButton>
+                    </div>
                   </section>
                 ))}
               </ScrollSpy>
             </div>
           </div>
-          <div className="service__Pagedetails__section__content__right">
-            <ScrollSpy
-              scrollThrottle={10}
-              useBoxMethod={false}
-              offsetTop={-250}
-            >
-              {Data?.map((item) => (
-                <section
-                  id={item.id}
-                  className="service__Pagedetails__section__content__right__content"
-                >
-                  <h2 className="service__Pagedetails__section__content__right__content__heading">
-                    {item.title}
-                  </h2>
-                  <p className="service__Pagedetails__section__content__right__content__info">
-                    {item.description}
-                  </p>
-                  <div className="service__Pagedetails__section__content__right__content__heading__links">
-                    <ul>
-                      <li>Approch</li>
-                      <li>Creativity</li>
-                      <li>Experienced</li>
-                    </ul>
-                    <ul>
-                      <li>Approch</li>
-                      <li>Creativity</li>
-                      <li>Experienced</li>
-                    </ul>
-                  </div>
-                  <MagnetButton>Get Started</MagnetButton>
-                </section>
-              ))}{" "}
-            </ScrollSpy>
-          </div>
         </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 }
 
