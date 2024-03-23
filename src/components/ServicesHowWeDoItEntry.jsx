b96c8fc0845c9624ebb540d1b717a2b5a535473c
M Abrar Khalid <100939524+abrarkhalidofficial@users.noreply.github.com>
2024-03-23 09:04:24 +0500

    Update ServicesHowWeDoItEntry.jsx

--- src/components/ServicesHowWeDoItEntry.jsx
+++ src/components/ServicesHowWeDoItEntry.jsx
@@ -1,24 +1,35 @@
+"use client";
+import { Fade } from "react-reveal";
+
 export default function ServicesHowWeDoItEntry({ icon, title, list }) {
   return (
     <div className="services__how__we__do__it__content__entry">
-      <div className="services__how__we__do__it__content__entry__icon heading">
-        {icon}
-      </div>
+      <Fade up delay={100}>
+        {" "}
+        <div className="services__how__we__do__it__content__entry__icon heading">
+          {icon}
+        </div>
+      </Fade>
       <div className="services__how__we__do__it__content__entry__card">
-        <div className="services__how__we__do__it__content__entry__card__heading heading">
-          {title}
-        </div>
-        <div className="services__how__we__do__it__content__entry__card__content">
-          {list.map((item, i) => (
-            <div
-              className="services__how__we__do__it__content__entry__card__content__entry"
-              key={i}
-            >
-              <div className="services__how__we__do__it__content__entry__card__content__entry__dot" />
-              {item}
-            </div>
-          ))}
-        </div>
+        <Fade up delay={300}>
+          {" "}
+          <div className="services__how__we__do__it__content__entry__card__heading heading">
+            {title}
+          </div>
+        </Fade>
+        <Fade up delay={500}>
+          <div className="services__how__we__do__it__content__entry__card__content">
+            {list.map((item, i) => (
+              <div
+                className="services__how__we__do__it__content__entry__card__content__entry"
+                key={i}
+              >
+                <div className="services__how__we__do__it__content__entry__card__content__entry__dot" />
+                {item}
+              </div>
+            ))}
+          </div>
+        </Fade>
       </div>
     </div>
   );
