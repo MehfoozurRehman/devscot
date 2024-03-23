30772eebd54704d58236ee36afb26dfaf04fab51
M Abrar Khalid <100939524+abrarkhalidofficial@users.noreply.github.com>
2024-03-23 09:04:36 +0500

    Update page.jsx

--- src/app/projects/[projectDetails]/page.jsx
+++ src/app/projects/[projectDetails]/page.jsx
@@ -9,6 +9,8 @@ import { useEffect, useState } from "react";
 import Footer from "@/components/Footer";
 import GetDemo from "@/components/GetDemo";
 import Header from "@/components/Header";
+import splitText from "@/utils/splitText";
+import { Fade } from "react-reveal";
 
 const aboutArray = [
   {
@@ -49,6 +51,7 @@ export default function ProjectDetails() {
     window.addEventListener("resize", checkWidth);
     return () => window.removeEventListener("resize", checkWidth);
   }, [swiperSlides]);
+  const heading = splitText("      UNITED MEAL");
 
   return (
     <>
@@ -58,38 +61,45 @@ export default function ProjectDetails() {
         <div className="project__details__text__header__container">
           <div className="project__details__text__header__container__left">
             <div className="project__details__text__header__container__left__heading">
-              UNITED MEAL
+              {heading}
             </div>
-            <div className="project__details__text__header__container__left__subheading">
-              Our client is a visionary company that aims to simplify
-              decentralized finance. With features offering convenience,
-              user-friendly crypto design, and multi-currency support, their
-              product — the Infinity wallet — seamlessly integrates into users’
-              lives, catering to all their financial needs.
-            </div>
-            <div className="project__details__text__header__container__left__btn">
-              <button style={{ margin: "0px" }} class="btn">
-                Contact us
-              </button>
-            </div>
+            <Fade up delay={100}>
+              {" "}
+              <div className="project__details__text__header__container__left__subheading">
+                Our client is a visionary company that aims to simplify
+                decentralized finance. With features offering convenience,
+                user-friendly crypto design, and multi-currency support, their
+                product — the Infinity wallet — seamlessly integrates into
+                users’ lives, catering to all their financial needs.
+              </div>
+            </Fade>{" "}
+            <Fade up delay={100}>
+              <div className="project__details__text__header__container__left__btn">
+                <button style={{ margin: "0px" }} class="btn">
+                  Contact us
+                </button>
+              </div>{" "}
+            </Fade>
           </div>
-          <div className="project__details__text__header__container__right">
-            <div className="project__details__text__header__container__right__logo__title">
-              CLIENT
+          <Fade up delay={100}>
+            <div className="project__details__text__header__container__right">
+              <div className="project__details__text__header__container__right__logo__title">
+                CLIENT
+              </div>
+              <div className="project__details__text__header__container__right__logo">
+                <img src="/maelogo.webp" alt="logo" />
+              </div>
+              <div className="project__details__right__text__services">
+                SERVICES WE PROVIDED
+              </div>
+              <div className="project__details__right__text__services__title">
+                UX/UI Design
+              </div>
+              <div className="project__details__right__text__services__title">
+                Graphic Design
+              </div>
             </div>
-            <div className="project__details__text__header__container__right__logo">
-              <img src="/maelogo.webp" alt="logo" />
-            </div>
-            <div className="project__details__right__text__services">
-              SERVICES WE PROVIDED
-            </div>
-            <div className="project__details__right__text__services__title">
-              UX/UI Design
-            </div>
-            <div className="project__details__right__text__services__title">
-              Graphic Design
-            </div>
-          </div>
+          </Fade>
         </div>
         <div className="project__header__imags__slider__over">
           <div className="project__header__imags__slider">
@@ -129,20 +139,29 @@ export default function ProjectDetails() {
         </div>
         <div className="project__details__text__header__container__bg" />
         <div className="project__details__about__container">
-          <div className="project__details__about__container__heading">
-            ABOUT THE PROJECT
-          </div>
+          <Fade up delay={100}>
+            {" "}
+            <div className="project__details__about__container__heading">
+              ABOUT THE PROJECT
+            </div>
+          </Fade>
           {aboutArray.map(({ heading, text }, index) => (
             <div
               className="project__details__about__container__text__wraper"
               key={index}
             >
-              <div className="project__details__about__container__text__wraper__heading">
-                {heading}
-              </div>
-              <div className="project__details__about__container__text__wraper__sub__heading">
-                {text}
-              </div>
+              <Fade up delay={100}>
+                {" "}
+                <div className="project__details__about__container__text__wraper__heading">
+                  {heading}
+                </div>
+              </Fade>
+              <Fade up delay={300}>
+                {" "}
+                <div className="project__details__about__container__text__wraper__sub__heading">
+                  {text}
+                </div>
+              </Fade>
             </div>
           ))}
         </div>
@@ -152,33 +171,45 @@ export default function ProjectDetails() {
             alt="devscot"
           />
         </div>
-        <div className="process__image">
-          <img
-            src="https://res.cloudinary.com/dsxbqyjwo/image/upload/v1701935150/Group_1707485878_mrsf2p.webp"
-            alt=""
-          />
-        </div>
-        <div className="Emphasizing__main__container">
-          <div className="Emphasizing__main__container__logo">
+        <Fade up delay={100}>
+          {" "}
+          <div className="process__image">
             <img
-              src="https://res.cloudinary.com/dsxbqyjwo/image/upload/v1701938382/Group_3_yjuhvx.webp"
-              alt="meal"
-            />
-          </div>
-          <div className="Emphasizing__main__container__heading">
-            Emphasizing consistency: creating a self-sufficient ecosystem within
-            Infinity
-          </div>
-          <div className="Emphasizing__main__container__subheading">
-            Our client is a visionary company that aims to simplify
-            decentralized finance. With features offering convenience
-          </div>
-          <div className="Emphasizing__main__container__project__img">
-            <img
-              src="https://res.cloudinary.com/dsxbqyjwo/image/upload/v1701937239/Group_1707485770_ge8de0.webp"
+              src="https://res.cloudinary.com/dsxbqyjwo/image/upload/v1701935150/Group_1707485878_mrsf2p.webp"
               alt=""
             />
           </div>
+        </Fade>
+        <div className="Emphasizing__main__container">
+          <Fade up delay={100}>
+            {" "}
+            <div className="Emphasizing__main__container__logo">
+              <img
+                src="https://res.cloudinary.com/dsxbqyjwo/image/upload/v1701938382/Group_3_yjuhvx.webp"
+                alt="meal"
+              />
+            </div>
+          </Fade>{" "}
+          <Fade up delay={100}>
+            <div className="Emphasizing__main__container__heading">
+              Emphasizing consistency: creating a self-sufficient ecosystem
+              within Infinity
+            </div>
+          </Fade>{" "}
+          <Fade up delay={300}>
+            <div className="Emphasizing__main__container__subheading">
+              Our client is a visionary company that aims to simplify
+              decentralized finance. With features offering convenience
+            </div>
+          </Fade>{" "}
+          <Fade up delay={500}>
+            <div className="Emphasizing__main__container__project__img">
+              <img
+                src="https://res.cloudinary.com/dsxbqyjwo/image/upload/v1701937239/Group_1707485770_ge8de0.webp"
+                alt=""
+              />
+            </div>
+          </Fade>
         </div>
         <div className="Emphasizing__main__container__bg__over">
           <div className="Emphasizing__main__container__bg">
@@ -225,129 +256,159 @@ export default function ProjectDetails() {
           </div>
         </div>
         <div className="guide__section__wraper">
-          <div className="guide__section__wraper__heading">
-            Visual Style Guide Creation
-          </div>
-          <div className="guide__section__wraper__sub__heading">
-            We created a visual style guide, which was a milestone in the design
-            consistency of the project. Just like the design system, it
-            encompassed all visual elements that can be used across the Infinity
-            platform.
-          </div>
+          <Fade up delay={100}>
+            {" "}
+            <div className="guide__section__wraper__heading">
+              Visual Style Guide Creation
+            </div>
+          </Fade>
+          <Fade up delay={300}>
+            {" "}
+            <div className="guide__section__wraper__sub__heading">
+              We created a visual style guide, which was a milestone in the
+              design consistency of the project. Just like the design system, it
+              encompassed all visual elements that can be used across the
+              Infinity platform.
+            </div>
+          </Fade>
           <div className="guide__section__card__wraper">
-            <div
-              className="guide__section__card"
-              style={{
-                backgroundColor: " #00CE6E",
-              }}
-            >
-              #00CE6E
-            </div>
-            <div
-              className="guide__section__card"
-              style={{
-                backgroundColor: " #98FFCF",
-                color: "BLACK",
-              }}
-            >
-              #98FFCF
-            </div>
-            <div
-              className="guide__section__card"
-              style={{
-                backgroundColor: " #33373B",
-              }}
-            >
-              #33373B
-            </div>
-            <div
-              className="guide__section__card"
-              style={{
-                backgroundColor: " #FFFFFF",
-                color: "BLACK",
-              }}
-            >
-              #FFFFFF
-            </div>
+            <Fade up delay={100}>
+              {" "}
+              <div
+                className="guide__section__card"
+                style={{
+                  backgroundColor: " #00CE6E",
+                }}
+              >
+                #00CE6E
+              </div>
+            </Fade>{" "}
+            <Fade up delay={100}>
+              <div
+                className="guide__section__card"
+                style={{
+                  backgroundColor: " #98FFCF",
+                  color: "BLACK",
+                }}
+              >
+                #98FFCF
+              </div>{" "}
+            </Fade>{" "}
+            <Fade up delay={100}>
+              <div
+                className="guide__section__card"
+                style={{
+                  backgroundColor: " #33373B",
+                }}
+              >
+                #33373B
+              </div>
+            </Fade>{" "}
+            <Fade up delay={100}>
+              <div
+                className="guide__section__card"
+                style={{
+                  backgroundColor: " #FFFFFF",
+                  color: "BLACK",
+                }}
+              >
+                #FFFFFF
+              </div>
+            </Fade>
           </div>
           <div className="guide__section__card__font">
-            <div className="guide__section__card__font__left">
-              <div className="guide__section__card__font__title">
-                Typography
+            <Fade up delay={300}>
+              <div className="guide__section__card__font__left">
+                <div className="guide__section__card__font__title">
+                  Typography
+                </div>
+                <div className="guide__section__card__font__left__name">
+                  Inter
+                </div>
+                <div className="guide__section__card__font__left__sub__name">
+                  Semi-bold
+                </div>
               </div>
-              <div className="guide__section__card__font__left__name">
-                Inter
+            </Fade>{" "}
+            <Fade up delay={600}>
+              <div className="guide__section__card__font__right">
+                <div className="guide__section__card__font__title">
+                  Heading Font
+                </div>
+                <div className="guide__section__card__font__right__name">
+                  Aa Bb Cc Dd Ee Ff Gg Hh li Kk LI Mm Nn Oo Pp Qq Rr Ss Tt Vv Xx
+                  Yy Zz
+                </div>
+                <div className="guide__section__card__font__right__sub__name">
+                  0123456789!@#$%*&*()_+
+                </div>
               </div>
-              <div className="guide__section__card__font__left__sub__name">
-                Semi-bold
-              </div>
-            </div>
-            <div className="guide__section__card__font__right">
-              <div className="guide__section__card__font__title">
-                Heading Font
-              </div>
-              <div className="guide__section__card__font__right__name">
-                Aa Bb Cc Dd Ee Ff Gg Hh li Kk LI Mm Nn Oo Pp Qq Rr Ss Tt Vv Xx
-                Yy Zz
-              </div>
-              <div className="guide__section__card__font__right__sub__name">
-                0123456789!@#$%*&*()_+
-              </div>
-            </div>
+            </Fade>
           </div>
         </div>
         <div className="result__container__over">
           <div className="result__container">
-            <div className="result__container__heading">THE RESULT</div>
+            <Fade up delay={100}>
+              {" "}
+              <div className="result__container__heading">THE RESULT</div>
+            </Fade>
             <div className="result__container__text__wraper__container">
-              <div className="result__container__text__wraper">
-                <div className="result__container__text__wraper__number">
-                  80%
+              <Fade up delay={200}>
+                <div className="result__container__text__wraper">
+                  <div className="result__container__text__wraper__number">
+                    80%
+                  </div>
+                  <div className="result__container__text__wraper__heading">
+                    User satisfaction rate
+                  </div>
+                  <div className="result__container__text__wraper__subheading">
+                    Our Web 3.0 development and design services left most users
+                    satisfied.
+                  </div>
                 </div>
-                <div className="result__container__text__wraper__heading">
-                  User satisfaction rate
+              </Fade>{" "}
+              <Fade up delay={200}>
+                <div className="result__container__text__wraper">
+                  <div className="result__container__text__wraper__number">
+                    40%
+                  </div>
+                  <div className="result__container__text__wraper__heading">
+                    User satisfaction rate
+                  </div>
+                  <div className="result__container__text__wraper__subheading">
+                    Our Web 3.0 development and design services left most users
+                    satisfied.
+                  </div>
                 </div>
-                <div className="result__container__text__wraper__subheading">
-                  Our Web 3.0 development and design services left most users
-                  satisfied.
+              </Fade>{" "}
+              <Fade up delay={500}>
+                <div className="result__container__text__wraper">
+                  <div className="result__container__text__wraper__number">
+                    25%
+                  </div>
+                  <div className="result__container__text__wraper__heading">
+                    User satisfaction rate
+                  </div>
+                  <div className="result__container__text__wraper__subheading">
+                    Our Web 3.0 development and design services left most users
+                    satisfied.
+                  </div>
                 </div>
-              </div>
-              <div className="result__container__text__wraper">
-                <div className="result__container__text__wraper__number">
-                  40%
+              </Fade>{" "}
+              <Fade up delay={500}>
+                <div className="result__container__text__wraper">
+                  <div className="result__container__text__wraper__number">
+                    5%
+                  </div>
+                  <div className="result__container__text__wraper__heading">
+                    User satisfaction rate
+                  </div>
+                  <div className="result__container__text__wraper__subheading">
+                    Our Web 3.0 development and design services left most users
+                    satisfied.
+                  </div>
                 </div>
-                <div className="result__container__text__wraper__heading">
-                  User satisfaction rate
-                </div>
-                <div className="result__container__text__wraper__subheading">
-                  Our Web 3.0 development and design services left most users
-                  satisfied.
-                </div>
-              </div>
-              <div className="result__container__text__wraper">
-                <div className="result__container__text__wraper__number">
-                  25%
-                </div>
-                <div className="result__container__text__wraper__heading">
-                  User satisfaction rate
-                </div>
-                <div className="result__container__text__wraper__subheading">
-                  Our Web 3.0 development and design services left most users
-                  satisfied.
-                </div>
-              </div>
-              <div className="result__container__text__wraper">
-                <div className="result__container__text__wraper__number">
-                  5%
-                </div>
-                <div className="result__container__text__wraper__heading">
-                  User satisfaction rate
-                </div>
-                <div className="result__container__text__wraper__subheading">
-                  Our Web 3.0 development and design services left most users
-                  satisfied.
-                </div>
-              </div>
+              </Fade>
             </div>
           </div>
         </div>
