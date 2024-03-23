13428bc6a6e01919c46bc767701adf45b5220341
M Abrar Khalid <100939524+abrarkhalidofficial@users.noreply.github.com>
2024-03-23 09:04:34 +0500

    Update page.jsx

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -1,5 +1,5 @@
+"use client";
 import "@/styles/projectpage.scss";
-
 import { ArrowRight } from "react-feather";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
@@ -10,51 +10,61 @@ const Data = [
     link: "/web-development",
     title: "Web development",
     img: "/FeatureSectionimg.webp",
+    delay: 100,
   },
   {
     link: "/mobile-development",
     title: "Mobile development",
     img: "/FeatureSectionimg.webp",
+    delay: 200,
   },
   {
     link: "/digital-marketing",
     title: "Digital Marketing",
     img: "/FeatureSectionimg.webp",
+    delay: 300,
   },
   {
     link: "/ui-ux-design",
     title: "UI/UX Design",
     img: "/FeatureSectionimg.webp",
+    delay: 400,
   },
   {
     link: "/branding",
     title: "Branding",
     img: "/FeatureSectionimg.webp",
+    delay: 500,
   },
   {
     link: "/seo",
     title: "E-commerce",
     img: "/FeatureSectionimg.webp",
+    delay: 600,
   },
   {
     link: "/seo",
     title: "NFT Marketplace",
     img: "/FeatureSectionimg.webp",
+    delay: 700,
   },
   {
     link: "/seo",
     title: "Wallet",
     img: "/FeatureSectionimg.webp",
+    delay: 800,
   },
   {
     link: "/seo",
     title: "Dapp",
     img: "/FeatureSectionimg.webp",
+    delay: 900,
   },
   {
     link: "/seo",
     title: "Defi",
     img: "/FeatureSectionimg.webp",
+    delay: 1000,
   },
 ];
 
@@ -88,21 +98,23 @@ export default function Projects() {
                   key={data.id}
                   className="projectpage__Section__content__card"
                 >
-                  <div className="projectpage__Section__content__card__content">
-                    <img
-                      className="projectpage__Section__content__card__content_img"
-                      src={data.img}
-                      alt="project"
-                    />
-                    <div className="projectpage__Section__content__card__content__name__icon">
-                      <div className="projectpage__Section__content__card__content__name">
-                        {data.title}
-                      </div>
-                      <div className="projectpage__Section__content__card__content__icon">
-                        <ArrowRight />
+                  <Fade up delay={data.delay ? data.delay : 100}>
+                    <div className="projectpage__Section__content__card__content">
+                      <img
+                        className="projectpage__Section__content__card__content_img"
+                        src={data.img}
+                        alt="project"
+                      />
+                      <div className="projectpage__Section__content__card__content__name__icon">
+                        <div className="projectpage__Section__content__card__content__name">
+                          {data.title}
+                        </div>
+                        <div className="projectpage__Section__content__card__content__icon">
+                          <ArrowRight />
+                        </div>
                       </div>
                     </div>
-                  </div>
+                  </Fade>
                 </a>
               );
             })}
