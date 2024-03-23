95c4ac792ea463b5b6b3be6d154ebbf9489aed98
M Abrar Khalid <100939524+abrarkhalidofficial@users.noreply.github.com>
2024-03-23 09:04:39 +0500

    Update page.jsx

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,66 +1,91 @@
+"use client";
+
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import splitText from "@/utils/splitText";
+import { Fade } from "react-reveal";
 
 export default function About() {
+  const heading = splitText(
+    "We’re a­ full­ service­ creative­ and­ digital­ agency,­ working­ globally­ with­ leargest ­brands"
+  );
   return (
     <>
       <Header />
       <div className="Aboutuspage">
-        <div className="Aboutuspage__heading">
-          We’re a­ full­ service­ creative­ and­ digital­ agency,­ working­
-          globally­ with­ leargest ­brands
-        </div>
+        <div className="Aboutuspage__heading">{heading}</div>
         <div className="Aboutuspage__about">
-          <button className="Aboutuspage__about__button">
-            Trend & Technology
-          </button>
-          <div className="Aboutuspage__about__info">
-            hink of the world's most iconica and successful brands easily
-            findable, impactful. Interactional and exceptional designs. Our
-            story takes beginning in 2013
-          </div>
-          <img
-            src="https://wealcoder.com/dev/html/axtra/assets/imgs/about/award.png"
-            alt=""
-            className="Aboutuspage__about__logo"
-          />
+          <Fade up delay={100}>
+            {" "}
+            <button className="Aboutuspage__about__button">
+              Trend & Technology
+            </button>{" "}
+          </Fade>
+          <Fade up delay={100}>
+            <div className="Aboutuspage__about__info">
+              hink of the world's most iconica and successful brands easily
+              findable, impactful. Interactional and exceptional designs. Our
+              story takes beginning in 2013
+            </div>{" "}
+          </Fade>
+          <Fade up delay={100}>
+            {" "}
+            <img
+              src="https://wealcoder.com/dev/html/axtra/assets/imgs/about/award.png"
+              alt=""
+              className="Aboutuspage__about__logo"
+            />{" "}
+          </Fade>
         </div>
-        <div className="Aboutuspage__video">
-          <video
-            loop={true}
-            autoPlay={true}
-            muted={true}
-            playsInline={true}
-            src="https://wealcoder.com/dev/html/axtra/assets/video/video.mp4"
-            className="Aboutuspage__video__video"
-          />
-        </div>
+        <Fade up delay={100}>
+          {" "}
+          <div className="Aboutuspage__video">
+            <video
+              loop={true}
+              autoPlay={true}
+              muted={true}
+              playsInline={true}
+              src="https://wealcoder.com/dev/html/axtra/assets/video/video.mp4"
+              className="Aboutuspage__video__video"
+            />
+          </div>{" "}
+        </Fade>
         <div className="Aboutuspage__ourstory">
           <div className="Aboutuspage__ourstory__content">
             <div className="Aboutuspage__ourstory__content__left">
-              <div className="Aboutuspage__ourstory__content__subheading">
-                Digital Studio
-              </div>
-              <div className="Aboutuspage__ourstory__content__heading">
-                Our Story
-              </div>{" "}
+              <Fade up delay={100}>
+                {" "}
+                <div className="Aboutuspage__ourstory__content__subheading">
+                  Digital Studio
+                </div>{" "}
+              </Fade>{" "}
+              <Fade up delay={100}>
+                <div className="Aboutuspage__ourstory__content__heading">
+                  Our Story
+                </div>{" "}
+              </Fade>
             </div>
             <div className="Aboutuspage__ourstory__content__right">
-              <div className="Aboutuspage__ourstory__content__right__info">
-                Your brand is the most important asset in your company let our
-                team of professionals help you with a good strategy took the
-                runway next with an edgy collection featuring dyed denim pieces.
-                The collection included patchwork denim, a trend that has
-                recently exploded in younger generations. Playing on aspects of
-                sustainability, the pieces appeared to be upcycled to establish
-                dimension and flair. This take on grunge and streetwear took
-                sustainable fashion to an entirely new level. <br />
-                <br />
-                ur specialized team of researchers, strategists, designers,
-                developers, and project managers work with streamlined processes
-                to break through organizational roadblocks. We translate
-                research into solutions, crafting thoughtful and unified brands.
-              </div>
+              <Fade up delay={100}>
+                {" "}
+                <div className="Aboutuspage__ourstory__content__right__info">
+                  Your brand is the most important asset in your company let our
+                  team of professionals help you with a good strategy took the
+                  runway next with an edgy collection featuring dyed denim
+                  pieces. The collection included patchwork denim, a trend that
+                  has recently exploded in younger generations. Playing on
+                  aspects of sustainability, the pieces appeared to be upcycled
+                  to establish dimension and flair. This take on grunge and
+                  streetwear took sustainable fashion to an entirely new level.{" "}
+                  <br />
+                  <br />
+                  ur specialized team of researchers, strategists, designers,
+                  developers, and project managers work with streamlined
+                  processes to break through organizational roadblocks. We
+                  translate research into solutions, crafting thoughtful and
+                  unified brands.
+                </div>{" "}
+              </Fade>
             </div>
           </div>
         </div>{" "}
@@ -70,117 +95,153 @@ export default function About() {
               <div className="Aboutuspage__numbers__content__card__number">
                 10k
               </div>
-              <div className="Aboutuspage__numbers__content__card__info">
-                Projects Completed
-              </div>
+              <Fade up delay={100}>
+                {" "}
+                <div className="Aboutuspage__numbers__content__card__info">
+                  Projects Completed
+                </div>
+              </Fade>
             </div>
             <div className="Aboutuspage__numbers__content__card">
               <div className="Aboutuspage__numbers__content__card__number">
                 18k
               </div>
-              <div className="Aboutuspage__numbers__content__card__info">
-                Happy customers
-              </div>
+              <Fade up delay={100}>
+                {" "}
+                <div className="Aboutuspage__numbers__content__card__info">
+                  Happy customers
+                </div>
+              </Fade>
             </div>
             <div className="Aboutuspage__numbers__content__card">
               <div className="Aboutuspage__numbers__content__card__number">
                 122
               </div>
-              <div className="Aboutuspage__numbers__content__card__info">
-                Years experiences
-              </div>
+              <Fade up delay={100}>
+                {" "}
+                <div className="Aboutuspage__numbers__content__card__info">
+                  Years experiences
+                </div>
+              </Fade>
             </div>
             <div className="Aboutuspage__numbers__content__card">
               <div className="Aboutuspage__numbers__content__card__number">
                 132
               </div>
-              <div className="Aboutuspage__numbers__content__card__info">
-                Awards achievement
-              </div>
+              <Fade up delay={100}>
+                {" "}
+                <div className="Aboutuspage__numbers__content__card__info">
+                  Awards achievement
+                </div>
+              </Fade>
             </div>
           </div>
         </div>
         <div className="Aboutuspage__logos">
           <div className="Aboutuspage__logos__content">
-            <div className="Aboutuspage__logos__content__subheading">
-              Internation Brands
-            </div>
-            <div className="Aboutuspage__logos__content__heading">
-              We are happy to work with global largest brands
-            </div>
+            <Fade up delay={100}>
+              {" "}
+              <div className="Aboutuspage__logos__content__subheading">
+                {splitText("Internation Brands")}
+              </div>
+            </Fade>
+            <Fade up delay={100}>
+              {" "}
+              <div className="Aboutuspage__logos__content__heading">
+                {splitText("We are happy to work with global largest brands")}
+              </div>
+            </Fade>
             <div className="Aboutuspage__logos__content__logos">
-              <div className="Aboutuspage__logos__content__logos__card">
-                <img
-                  src="https://wealcoder.com/dev/html/axtra/assets/imgs/brand/1.png"
-                  alt=""
-                  className="Aboutuspage__logos__content__logos__card__image"
-                />
-              </div>
-
-              <div className="Aboutuspage__logos__content__logos__card">
-                <img
-                  src="https://wealcoder.com/dev/html/axtra/assets/imgs/brand/1.png"
-                  alt=""
-                  className="Aboutuspage__logos__content__logos__card__image"
-                />
-              </div>
-
-              <div className="Aboutuspage__logos__content__logos__card">
-                <img
-                  src="https://wealcoder.com/dev/html/axtra/assets/imgs/brand/1.png"
-                  alt=""
-                  className="Aboutuspage__logos__content__logos__card__image"
-                />
-              </div>
-              <div className="Aboutuspage__logos__content__logos__card">
-                <img
-                  src="https://wealcoder.com/dev/html/axtra/assets/imgs/brand/1.png"
-                  alt=""
-                  className="Aboutuspage__logos__content__logos__card__image"
-                />
-              </div>
-              <div className="Aboutuspage__logos__content__logos__card">
-                <img
-                  src="https://wealcoder.com/dev/html/axtra/assets/imgs/brand/1.png"
-                  alt=""
-                  className="Aboutuspage__logos__content__logos__card__image"
-                />
-              </div>
-              <div className="Aboutuspage__logos__content__logos__card">
-                <img
-                  src="https://wealcoder.com/dev/html/axtra/assets/imgs/brand/1.png"
-                  alt=""
-                  className="Aboutuspage__logos__content__logos__card__image"
-                />
-              </div>
-              <div className="Aboutuspage__logos__content__logos__card">
-                <img
-                  src="https://wealcoder.com/dev/html/axtra/assets/imgs/brand/1.png"
-                  alt=""
-                  className="Aboutuspage__logos__content__logos__card__image"
-                />
-              </div>
-              <div className="Aboutuspage__logos__content__logos__card">
-                <img
-                  src="https://wealcoder.com/dev/html/axtra/assets/imgs/brand/1.png"
-                  alt=""
-                  className="Aboutuspage__logos__content__logos__card__image"
-                />
-              </div>
-              <div className="Aboutuspage__logos__content__logos__card">
-                <img
-                  src="https://wealcoder.com/dev/html/axtra/assets/imgs/brand/1.png"
-                  alt=""
-                  className="Aboutuspage__logos__content__logos__card__image"
-                />
-              </div>
-              <div className="Aboutuspage__logos__content__logos__card">
-                <img
-                  src="https://wealcoder.com/dev/html/axtra/assets/imgs/brand/1.png"
-                  alt=""
-                  className="Aboutuspage__logos__content__logos__card__image"
-                />
-              </div>
+              <Fade up delay={100}>
+                <div className="Aboutuspage__logos__content__logos__card">
+                  <img
+                    src="https://wealcoder.com/dev/html/axtra/assets/imgs/brand/1.png"
+                    alt=""
+                    className="Aboutuspage__logos__content__logos__card__image"
+                  />
+                </div>
+              </Fade>
+              <Fade up delay={200}>
+                <div className="Aboutuspage__logos__content__logos__card">
+                  <img
+                    src="https://wealcoder.com/dev/html/axtra/assets/imgs/brand/1.png"
+                    alt=""
+                    className="Aboutuspage__logos__content__logos__card__image"
+                  />
+                </div>
+              </Fade>
+              <Fade up delay={300}>
+                <div className="Aboutuspage__logos__content__logos__card">
+                  <img
+                    src="https://wealcoder.com/dev/html/axtra/assets/imgs/brand/1.png"
+                    alt=""
+                    className="Aboutuspage__logos__content__logos__card__image"
+                  />
+                </div>
+              </Fade>
+              <Fade up delay={400}>
+                <div className="Aboutuspage__logos__content__logos__card">
+                  <img
+                    src="https://wealcoder.com/dev/html/axtra/assets/imgs/brand/1.png"
+                    alt=""
+                    className="Aboutuspage__logos__content__logos__card__image"
+                  />
+                </div>
+              </Fade>
+              <Fade up delay={500}>
+                <div className="Aboutuspage__logos__content__logos__card">
+                  <img
+                    src="https://wealcoder.com/dev/html/axtra/assets/imgs/brand/1.png"
+                    alt=""
+                    className="Aboutuspage__logos__content__logos__card__image"
+                  />
+                </div>
+              </Fade>
+              <Fade up delay={600}>
+                <div className="Aboutuspage__logos__content__logos__card">
+                  <img
+                    src="https://wealcoder.com/dev/html/axtra/assets/imgs/brand/1.png"
+                    alt=""
+                    className="Aboutuspage__logos__content__logos__card__image"
+                  />
+                </div>
+              </Fade>
+              <Fade up delay={700}>
+                <div className="Aboutuspage__logos__content__logos__card">
+                  <img
+                    src="https://wealcoder.com/dev/html/axtra/assets/imgs/brand/1.png"
+                    alt=""
+                    className="Aboutuspage__logos__content__logos__card__image"
+                  />
+                </div>
+              </Fade>
+              <Fade up delay={800}>
+                <div className="Aboutuspage__logos__content__logos__card">
+                  <img
+                    src="https://wealcoder.com/dev/html/axtra/assets/imgs/brand/1.png"
+                    alt=""
+                    className="Aboutuspage__logos__content__logos__card__image"
+                  />
+                </div>
+              </Fade>
+              <Fade up delay={900}>
+                <div className="Aboutuspage__logos__content__logos__card">
+                  <img
+                    src="https://wealcoder.com/dev/html/axtra/assets/imgs/brand/1.png"
+                    alt=""
+                    className="Aboutuspage__logos__content__logos__card__image"
+                  />
+                </div>
+              </Fade>
+              <Fade up delay={1000}>
+                <div className="Aboutuspage__logos__content__logos__card">
+                  <img
+                    src="https://wealcoder.com/dev/html/axtra/assets/imgs/brand/1.png"
+                    alt=""
+                    className="Aboutuspage__logos__content__logos__card__image"
+                  />
+                </div>
+              </Fade>
             </div>
           </div>
         </div>
