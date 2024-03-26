13e09d243705a4fdedcca29e1ac5e081a3f10245
M Abrar Khalid <100939524+abrarkhalidofficial@users.noreply.github.com>
2024-03-26 12:59:13 +0500

    Update ClientLayout.js

--- src/app/ClientLayout.js
+++ src/app/ClientLayout.js
@@ -1,9 +1,9 @@
-'use client'
+"use client";
 
 import { useEffect, useState } from "react";
+import { GridLoader } from "react-spinners";
 
 export default function ClientLayout({ children }) {
-
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
@@ -13,10 +13,32 @@ export default function ClientLayout({ children }) {
     }, []);
 
     return (
-        <>{loading &&
-            <div>loading</div>}
-            {children}
+        <>
+            {loading && (
+                <div
+                    className="loadingscreen"
+                    style={{
+                        width: "100%",
+                        height: "100%",
+                        backgroundColor: "#000",
+                        position: "fixed",
+                        top: 0,
+                        left: 0,
+                        right: 0,
+                        bottom: 0,
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        zIndex: 99,
+                    }}
+                >
+                    <GridLoader
+                        loading={true}
 
+                        color="#139948" />
+                </div>
+            )}
+            {children}
         </>
     );
-};
+}
