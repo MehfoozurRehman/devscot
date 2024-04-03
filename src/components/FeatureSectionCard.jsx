4ec640fd1526f41c322aaab21779cf31917aab80
M Abrar Khalid <100939524+abrarkhalidofficial@users.noreply.github.com>
2024-04-03 12:17:11 +0500

    Update FeatureSectionCard.jsx

--- src/components/FeatureSectionCard.jsx
+++ src/components/FeatureSectionCard.jsx
@@ -7,34 +7,74 @@ import { motion } from "framer-motion";
 export default function FeatureSectionCard() {
   const Data = [
     {
+      delay: 100,
       img: "/project1img.png",
-      link: "/projects/meetworth",
-      heading: "Meet Worth",
+      link: "projects/meetworth",
+      title: "Meet Worth",
       info: "Business investor finder app: Streamlining connections between entrepreneurs and investors for efficient funding opportunities.",
     },
     {
-      link: "/projects/mark",
+      delay: 200,
+      link: "projects/mark",
       img: "/project2img.png",
-      heading: "Mark",
+      title: "Mark",
       info: "Banking app: Securely manage finances, transfer funds, pay bills, and access banking services conveniently from your mobile device.",
     },
     {
-      link: "/projects/unitedmeal",
+      delay: 300,
+      link: "projects/unitedmeal",
       img: "/project3img.png",
-      heading: "United Meal",
+      title: "United Meal",
       info: "Food app: Discover, order, and enjoy a diverse range of cuisines from local restaurants, with options for delivery, pickup, and reservations.",
     },
     {
-      link: "/projects/sakenemaar",
+      delay: 400,
+      link: "projects/sakenemaar",
       img: "/project4img.png",
-      heading: "Saken Emaar",
+      title: "Saken Emaar",
       info: "Real estate  Search, buy, sell, or rent properties, access market insights, and connect with agents for seamless property transactions.",
     },
     {
-      link: "/projects/muslimbiz",
+      delay: 500,
+      link: "projects/muslimbiz",
       img: "/project5img.png",
-      heading: "Muslim Biz",
+      title: "Muslim Biz",
       info: "Hiring labor web platform: Facilitating the recruitment process, connecting employers with skilled workers, and streamlining labor management for various industries.",
+    },
+    {
+      delay: 600,
+      link: "projects/walking",
+      img: "/walkingbanner.png",
+      title: "Walking App",
+      info: "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim",
+    },
+    {
+      delay: 700,
+      link: "projects/daltaswap",
+      img: "/daltabanner.png",
+      title: "Dalta Swap ",
+      info: "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim",
+    },
+    {
+      delay: 800,
+      link: "projects/burns",
+      img: "/burnsbanner.png",
+      title: "Burns ",
+      info: "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim",
+    },
+    {
+      delay: 900,
+      link: "projects/healthvital",
+      img: "/healthbanner.png",
+      title: "Health Vital ",
+      info: "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim",
+    },
+    {
+      delay: 1000,
+      link: "projects/weekly",
+      img: "/weeklybanner.png",
+      title: "Weekly ",
+      info: "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim",
     },
   ];
   return Data.map((data) => (
@@ -54,7 +94,7 @@ export default function FeatureSectionCard() {
           className="FeatureSection__content__right__content__card"
         >
           <div className="FeatureSection__content__right__content__card__heading">
-            {data.heading}
+            {data.title}
             <div className="FeatureSection__content__right__content__card__arrow">
               <ArrowUpRight />
             </div>
