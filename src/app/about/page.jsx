9da7c2090e68e4a74352cd0fc350133bb2a1d181
MehfoozurRehman <mehfoozijaz786@gmail.com>
2024-04-27 13:53:07 +0500

    Update page.jsx

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -5,27 +5,28 @@ import Footer from "@/components/Footer";
 import Footermobile from "@/components/Footermobile";
 import Header from "@/components/Header";
 import splitText from "@/utils/splitText";
+import { stats } from "@/db/stats";
 
 export default function About() {
   return (
     <>
       <Header />
       <div className="Aboutuspage">
-        <div className="Aboutuspage__heading">
-          We’re a­ full­ service­ creative­ and­ digital­ agency,­ working­
-          globally­ with­ leargest ­brands
-        </div>
+        <div className="Aboutuspage__heading">Let us introduce ourselves</div>
         <div className="Aboutuspage__about">
           <Fade up delay={100}>
-            <button className="Aboutuspage__about__button">
-              Trend & Technology
+            <button
+              onClick={() => (window.location.href = "/contact")}
+              className="Aboutuspage__about__button"
+            >
+              Lets Talk
             </button>
           </Fade>
           <Fade up delay={100}>
             <div className="Aboutuspage__about__info">
-              hink of the world's most iconica and successful brands easily
-              findable, impactful. Interactional and exceptional designs. Our
-              story takes beginning in 2013
+              We are a team of seasoned engineers, designers, and strategists
+              who work together to create cutting-edge software solutions for
+              businesses that drive results.
             </div>
           </Fade>
           <Fade up delay={100}>
@@ -65,21 +66,19 @@ export default function About() {
             <div className="Aboutuspage__ourstory__content__right">
               <Fade up delay={100}>
                 <div className="Aboutuspage__ourstory__content__right__info">
-                  Your brand is the most important asset in your company let our
-                  team of professionals help you with a good strategy took the
-                  runway next with an edgy collection featuring dyed denim
-                  pieces. The collection included patchwork denim, a trend that
-                  has recently exploded in younger generations. Playing on
-                  aspects of sustainability, the pieces appeared to be upcycled
-                  to establish dimension and flair. This take on grunge and
-                  streetwear took sustainable fashion to an entirely new level.
+                  We are a software development company that provides cutting
+                  edge engineering solutions, helping local and international
+                  companies and clients untangle complex issues that always
+                  emerge during their digital evolution journey. Since 2013 we
+                  have been a visionary and a reliable software engineering
+                  partner for world-class brands.
                   <br />
                   <br />
-                  ur specialized team of researchers, strategists, designers,
-                  developers, and project managers work with streamlined
-                  processes to break through organizational roadblocks. We
-                  translate research into solutions, crafting thoughtful and
-                  unified brands.
+                  Our software development services include consulting and
+                  product engineering, mobile app development, web development.
+                  We are a team of seasoned engineers, designers, and
+                  strategists who work together to create cutting-edge software
+                  solutions for businesses that drive results.
                 </div>
               </Fade>
             </div>
@@ -87,46 +86,21 @@ export default function About() {
         </div>
         <div className="Aboutuspage__numbers">
           <div className="Aboutuspage__numbers__content">
-            <div className="Aboutuspage__numbers__content__card">
-              <div className="Aboutuspage__numbers__content__card__number">
-                10k
-              </div>
-              <Fade up delay={100}>
-                <div className="Aboutuspage__numbers__content__card__info">
-                  Projects Completed
+            {stats.map((entry) => (
+              <div
+                key={entry.text}
+                className="Aboutuspage__numbers__content__card"
+              >
+                <div className="Aboutuspage__numbers__content__card__number">
+                  {entry.number}+
                 </div>
-              </Fade>
-            </div>
-            <div className="Aboutuspage__numbers__content__card">
-              <div className="Aboutuspage__numbers__content__card__number">
-                18k
+                <Fade up delay={100}>
+                  <div className="Aboutuspage__numbers__content__card__info">
+                    {entry.text}
+                  </div>
+                </Fade>
               </div>
-              <Fade up delay={100}>
-                <div className="Aboutuspage__numbers__content__card__info">
-                  Happy customers
-                </div>
-              </Fade>
-            </div>
-            <div className="Aboutuspage__numbers__content__card">
-              <div className="Aboutuspage__numbers__content__card__number">
-                122
-              </div>
-              <Fade up delay={100}>
-                <div className="Aboutuspage__numbers__content__card__info">
-                  Years experiences
-                </div>
-              </Fade>
-            </div>
-            <div className="Aboutuspage__numbers__content__card">
-              <div className="Aboutuspage__numbers__content__card__number">
-                132
-              </div>
-              <Fade up delay={100}>
-                <div className="Aboutuspage__numbers__content__card__info">
-                  Awards achievement
-                </div>
-              </Fade>
-            </div>
+            ))}
           </div>
         </div>
         <div className="Aboutuspage__logos">
