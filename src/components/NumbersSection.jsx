593d9f1afb7508d7312bb1904cfe028a7ccefcbf
MehfoozurRehman <mehfoozijaz786@gmail.com>
2024-04-27 13:53:10 +0500

    Update NumbersSection.jsx

--- src/components/NumbersSection.jsx
+++ src/components/NumbersSection.jsx
@@ -2,30 +2,8 @@
 
 import CountUpWhenInView from "./CountUpWhenInView";
 import Fade from "@/components/Fade";
+import { stats } from "../db/stats";
 import { useState } from "react";
-
-const stats = [
-  {
-    number: "200",
-    text: "Projects Completed",
-    delay: "100",
-  },
-  {
-    number: "50",
-    text: "Satisfied Customers",
-    delay: "500",
-  },
-  {
-    number: "8",
-    text: "Years of Experience",
-    delay: "800",
-  },
-  {
-    number: "5",
-    text: "Awards & Achievements",
-    delay: "1200",
-  },
-];
 
 export default function NumbersSection() {
   const [isMouseHover, setIsMouseHover] = useState(2);
