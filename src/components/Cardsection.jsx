41ddf10dd620a9c30964487dff2956d678cd27d3
MehfoozurRehman <mehfoozijaz786@gmail.com>
2024-04-27 15:15:33 +0500

    Update Cardsection.jsx

--- src/components/Cardsection.jsx
+++ src/components/Cardsection.jsx
@@ -4,47 +4,18 @@ import "@/styles/style.scss";
 
 import { motion, useScroll, useTransform } from "framer-motion";
 
-import Fade from "@/components/Fade";
+import HorizontalScrollCarouselCard from "./HorizontalScrollCarouselCard";
 import { testimonials } from "../db/testimonials";
 import { useRef } from "react";
 
-const HorizontalScrollCarouselCard = ({ card }) => {
-  return (
-    <>
-      <div className="card__main__div">
-        <div className="card__main__div__circle"></div>
-        <div className="card__main__div__circle__two"></div>
-      </div>
-
-      <div
-        key={card.id}
-        className="classs group relative h-[450px] w-[450px] overflow-hidden bg-neutral-200"
-      >
-        <div className="card__content absolute inset-0 z-10 grid place-content-center">
-          <Fade up delay={100}>
-            <div className="card__heading">{card.title}</div>
-          </Fade>
-          <div className="card__content__info__number">
-            <Fade up delay={100}>
-              <p className="card__info">{card.quote}</p>
-            </Fade>
-            <Fade up delay={100}>
-              <div className="card__number">{card.number}</div>
-            </Fade>
-          </div>
-        </div>
-      </div>
-    </>
-  );
-};
-
-const HorizontalScrollCarousel = () => {
+export default function HorizontalScrollCarousel() {
   const targetRef = useRef(null);
+
   const { scrollYProgress } = useScroll({
     target: targetRef,
   });
 
-  const x = useTransform(scrollYProgress, [0, 1], ["-95%", "1%"]);
+  const x = useTransform(scrollYProgress, [0, 1], ["-53%", "1%"]);
 
   return (
     <div className="bg-neutral-800">
@@ -59,6 +30,4 @@ const HorizontalScrollCarousel = () => {
       </section>
     </div>
   );
-};
-
-export default HorizontalScrollCarousel;
+}
