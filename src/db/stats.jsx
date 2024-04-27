2c93bec97a6e32a3b07f923f23bf8cb25fb6af41
MehfoozurRehman <mehfoozijaz786@gmail.com>
2024-04-27 13:53:15 +0500

    Create stats.jsx

---
+++ src/db/stats.jsx
@@ -0,0 +1,23 @@
+"use client";
+export const stats = [
+  {
+    number: "200",
+    text: "Projects Completed",
+    delay: "100",
+  },
+  {
+    number: "50",
+    text: "Satisfied Customers",
+    delay: "500",
+  },
+  {
+    number: "8",
+    text: "Years of Experience",
+    delay: "800",
+  },
+  {
+    number: "5",
+    text: "Awards & Achievements",
+    delay: "1200",
+  },
+];
