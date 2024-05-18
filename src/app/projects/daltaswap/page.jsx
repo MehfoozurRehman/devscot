0c47ce08e1f5fae2bd19e554446ea7ad47e3a766
M Abrar Khalid <iabrarkhalidofficial@gmail.com>
2024-05-18 11:42:31 +0500

    Update page.jsx

--- src/app/projects/daltaswap/page.jsx
+++ src/app/projects/daltaswap/page.jsx
@@ -110,7 +110,10 @@ export default function ProjectDetails() {
             />
           </div>
         </div>
-        <div className="project__details__text__header__container__bg" />
+   
+        <div className="project__details__text__header__container__bg" style={{
+            background: 'linear-gradient(120deg, #000622 3.95%, #4b1858 96.98%)'
+        }}/>
         <div className="project__details__about__container">
           <Fade up delay={100}>
             <div className="project__details__about__container__heading">
