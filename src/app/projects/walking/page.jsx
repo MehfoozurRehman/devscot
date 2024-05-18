6f3d8a5e13a2d5290aa0c2fa6d080cf4b56c1a5c
M Abrar Khalid <iabrarkhalidofficial@gmail.com>
2024-05-18 11:42:05 +0500

    Update page.jsx

--- src/app/projects/walking/page.jsx
+++ src/app/projects/walking/page.jsx
@@ -150,7 +150,9 @@ export default function ProjectDetails() {
             </Swiper>
           </div>
         </div>
-        <div className="project__details__text__header__container__bg" />
+        <div className="project__details__text__header__container__bg" style={{
+            background: 'linear-gradient(120deg, #130032 3.95%, #da3667 96.98%)'
+        }}/>
         <div className="project__details__about__container">
           <Fade up delay={100}>
             <div className="project__details__about__container__heading">
