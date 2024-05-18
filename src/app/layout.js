79af6403746d34df057cf86d1e6c8d0e68a3aa20
M Abrar Khalid <iabrarkhalidofficial@gmail.com>
2024-05-18 10:53:21 +0500

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,10 +4,7 @@ import Loader from "./Loader";
 import { LoaderTagline } from "./LoaderTagline";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
-export const metadata = {
-  title: "Devscot",
-  description: "A software house based in Pakistan",
-};
+
 
 export default function RootLayout({ children }) {
   return (
