586b673241305d4e87629b0e235872e5fbd2280c
M Abrar Khalid <iabrarkhalidofficial@gmail.com>
2024-06-11 12:46:21 +0500

    Create HomeSectionHeadingmobile.jsx

---
+++ src/components/HomeSectionHeadingmobile.jsx
@@ -0,0 +1,9 @@
+"use client";
+export default function HomeSectionHeadingmobile({}) {
+
+
+  return <div className="hero__section__content__heading__mobile">
+
+   DESIGN. <br /> DEVELOPMENT <br />   & IDENTITY  FOR YOUR  PRODUCT
+  </div>;
+}
