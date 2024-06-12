202e49c180126dfde55b17d3df648575d21d0d93
M Abrar Khalid <iabrarkhalidofficial@gmail.com>
2024-06-12 17:29:27 +0500

    Create data.js

---
+++ src/components/Headers/Nav/data.js
@@ -0,0 +1,41 @@
+export const links = [
+    {
+        title: "Home",
+        href: "/"
+    },
+    {
+        title: "Services",
+        href: "/services"
+    },
+    {
+        title: "Portfolio",
+        href: "/projects"
+    },
+    {
+        title: "About Us",
+        href: "/about"
+    },
+    {
+        title: "Contact Us",
+        href: "/contact"
+    }
+]
+
+export const footerLinks = [
+    {
+        title: "Facebook",
+        href: "/"
+    },
+    {
+        title: "LinkedIn",
+        href: "/"
+    },
+    {
+        title: "Instagram",
+        href: "/"
+    },
+    {
+        title: "Twitter",
+        href: "/"
+    }
+]
