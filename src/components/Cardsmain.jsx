982024cd797ec51a3d98f980b2c7c06b50f0fa98
M Abrar Khalid <iabrarkhalidofficial@gmail.com>
2024-06-12 17:29:47 +0500

    Create Cardsmain.jsx

---
+++ src/components/Cardsmain.jsx
@@ -0,0 +1,78 @@
+"use client";
+
+
+export default function Cardsmain({}) {
+  return (
+    <>
+    <section className="Cardsmain__section">
+
+   
+      <div className="Cardsmain__section__main">
+        <div className="Cardsmain__section__content">
+          <div className="Cardsmain__section__content__number__text">
+            <div className="Cardsmain__section__content__number__text__number">
+              01
+            </div>
+            <div className="Cardsmain__section__content__number__text__text">
+              Web Development
+            </div>
+          </div>
+          <div className="Cardsmain__section__content__text">
+            We create websites that are visually appealing, user-friendly, and
+            responsive. Our team of web developers is experienced in creating
+            websites that are easy to navigate and optimized for search
+            engines.
+          </div>
+        </div>
+        <div className="Cardsmain__section__content">
+          <div className="Cardsmain__section__content__number__text">
+            <div className="Cardsmain__section__content__number__text__number">
+              02
+            </div>
+            <div className="Cardsmain__section__content__number__text__text">
+              Digital Marketing
+            </div>
+          </div>
+          <div className="Cardsmain__section__content__text">
+            We offer a range of digital marketing services to help you reach your
+            target audience and grow your business. Our team of digital
+            marketers is experienced in creating campaigns that drive results.
+          </div>
+        </div>
+      </div>
+      <div className="Cardsmain__section__main">
+        <div className="Cardsmain__section__content">
+          <div className="Cardsmain__section__content__number__text">
+            <div className="Cardsmain__section__content__number__text__number">
+              03
+            </div>
+            <div className="Cardsmain__section__content__number__text__text">
+              Web Development
+            </div>
+          </div>
+          <div className="Cardsmain__section__content__text">
+            We create websites that are visually appealing, user-friendly, and
+            responsive. Our team of web developers is experienced in creating
+            websites that are easy to navigate and optimized for search
+            engines.
+          </div>
+        </div>
+        <div className="Cardsmain__section__content">
+          <div className="Cardsmain__section__content__number__text">
+            <div className="Cardsmain__section__content__number__text__number">
+              04
+            </div>
+            <div className="Cardsmain__section__content__number__text__text">
+              Digital Marketing
+            </div>
+          </div>
+          <div className="Cardsmain__section__content__text">
+            We offer a range of digital marketing services to help you reach your
+            target audience and grow your business. Our team of digital
+            marketers is experienced in creating campaigns that drive results.
+          </div>
+        </div>
+      </div> </section>
+    </>
+  );
+}
