0b99e3bcd5479928918005444209b7d5ff5f6a45
M Abrar Khalid <iabrarkhalidofficial@gmail.com>
2024-06-13 15:57:50 +0500

    Update page.jsx

--- src/app/projects/sakenemaar/page.jsx
+++ src/app/projects/sakenemaar/page.jsx
@@ -16,15 +16,15 @@ import splitText from "@/utils/splitText";
 const aboutArray = [
   {
     heading: "The client",
-    text: "Our client is a visionary company that aims to simplify decentralized finance. With features offering convenience, user-friendly crypto design, and multi-currency support, their product — the Infinity wallet — seamlessly integrates into users’ lives, catering to all their financial needs.",
+    text: "DevScot proudly collaborated with SAKEN EMAAR, a distinguished real estate platform committed to revolutionizing property transactions. Our partnership centered on crafting a user interface (UI) design that enhances user experience and facilitates seamless property exploration. SAKEN EMAAR’s vision resonates with DevScot’s dedication to delivering innovative UI/UX solutions that prioritize functionality and aesthetic appeal. Through extensive consultations and a deep understanding of SAKEN EMAAR’s brand ethos, we aimed to create a UI design that reflects their commitment to excellence in real estate services.",
   },
   {
     heading: "Challenge",
-    text: "The diverse and expansive functionality of the Infinity decentralized mobile app posed a complex challenge to our team. The Web 3 design demanded our attention across numerous screens to capture meaningful user interactions. Other hurdles included reinforcing security measures, offering versatile wallet control options, and enabling Infinity to replace multiple smaller applications.",
+    text: "Designing for SAKEN EMAAR presented unique challenges that demanded strategic solutions. These included integrating a vast array of property listings while maintaining intuitive navigation, ensuring data security and privacy in real estate transactions, and optimizing the platform for both desktop and mobile users. Additionally, incorporating advanced search functionalities and interactive property visualization tools posed technical complexities. DevScot tackled these challenges through meticulous research, iterative design processes, and collaboration with SAKEN EMAAR’s team to ensure the UI design meets industry standards and surpasses user expectations.",
   },
   {
     heading: "Our solution",
-    text: "Our goal evolved around the Infinity wallet’s consistency. We aimed to work out styles, fonts, and colors and compile a cohesive set of icons and elements for a seamless user experience across all pages. Our experts also focused on enhancing user-friendliness, reducing visual clutter, facilitating onboarding for beginners, and delivering all DeFi mobile app features properly.",
+    text: "DevScot approached the UI design for SAKEN EMAAR with a comprehensive strategy rooted in user-centric principles. We began by conducting extensive user research and usability testing to understand user behaviors and preferences. This informed our design decisions, enabling us to create a clean and intuitive interface that simplifies property browsing and transaction processes. Leveraging modern UI design trends, we implemented interactive maps, high-resolution imagery, and detailed property descriptions to enhance user engagement and decision-making. Through continuous iteration and feedback loops with SAKEN EMAAR, we refined the UI design to achieve optimal usability, visual appeal, and performance across all devices. The result is a sophisticated and user-friendly UI for SAKEN EMAAR that elevates the real estate experience and strengthens their position as a leading platform in the industry.",
   },
 ];
 
@@ -66,8 +66,13 @@ export default function ProjectDetails() {
             </div>
             <Fade up delay={100}>
               <div className="project__details__text__header__container__left__subheading">
-                Sken Emaar is a construction company that provides services like
-                building construction, road construction, and many more.
+              Introducing Saken Emaar, your premier real estate website offering a comprehensive platform to buy, sell, and rent properties. Explore diverse listings, from luxurious homes to commercial spaces, with detailed property information and virtual tours. Experience seamless transactions and expert guidance in real estate, tailored to meet your housing and investment needs.
+
+
+
+
+
+
               </div>
             </Fade>
             <Fade up delay={100}>
